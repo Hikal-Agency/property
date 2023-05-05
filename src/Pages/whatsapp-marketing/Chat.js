f370ad60e894f6ee8bb7f8b60d818523490b226f
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-05-05 04:55:26 -0700

    fetching contacts

--- src/Pages/whatsapp-marketing/Chat.js
+++ src/Pages/whatsapp-marketing/Chat.js
@@ -1,6 +1,13 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import { Box, CircularProgress , Button, Select, MenuItem, TextField} from "@mui/material";
+import { useEffect, useState } from "react";
+import {QRCodeCanvas} from 'qrcode.react';
+import {
+  Box,
+  CircularProgress,
+  Button,
+  Select,
+  MenuItem,
+  TextField,
+} from "@mui/material";
 import { io } from "socket.io-client";
 import axios from "axios";
 
@@ -10,10 +17,11 @@ const Chat = () => {
   const [qr, setQr] = useState("");
   const [data, setData] = useState([]);
   const [messageValue, setMessageValue] = useState("");
-  const [contactValue, setContactValue] = useState("");
+  const [contactValue, setContactValue] = useState("0");
+  const [selectedContact, setSelectedContact] = useState(null);
+  const [serverDisconnected, setServerDisconnected] = useState(false);
 
-const socketURL = "http://localhost:5000";
-    const socket = io(socketURL);
+  const socketURL = "http://localhost:5000";
 
   const handleInput = (e) => {
     setMessageValue(e.target.value);
@@ -23,71 +31,148 @@ const socketURL = "http://localhost:5000";
     setContactValue(e.target.value);
   };
 
+  const selectContact = (contact) => {
+    setSelectedContact(contact);
+  };
+
   const handleSend = async () => {
     await axios.post(`${socketURL}/send-message`, {
-        to: contactValue + "@c.us",
-        msg: messageValue,
+      to: contactValue + "@c.us",
+      msg: messageValue,
     });
-  }
+  };
 
   useEffect(() => {
+    const socket = io(socketURL);
 
-    setloading(true);
-    socket.on("get_qr", (data) => {
-      const qrCode = data;
-      setQr(qrCode);
-      setloading(false);
-    });
+    socket.on("connect", () => {
+        console.log("Client Connected")
+      setServerDisconnected(false);
+      setloading(true);
+      socket.on("get_qr", (data) => {
+        const qrCode = data;
+        setQr(qrCode);
 
-    socket.on("user_disconnected", () => {
-        alert("Disconnected Device")
+        console.log("QR REceived")
+        setloading(false);
+      });
+
+      socket.on("user_disconnected", () => {
+        alert("Disconnected Device");
         document.location.reload();
-    })
+      });
 
-    socket.on("user_ready", (data) => {
-      setReady(true);
-      setData(data);
-      setloading(false);
+      socket.on("user_ready", async (clientInfo) => {
+        setReady(true);
+        setloading(false);
+
+        console.log("User ready")
+
+        const contacts = await axios.get(`${socketURL}/user-contacts`);
+        const profileImage = await axios.get(`${socketURL}/user-profilepic`);
+
+        setData({
+          userInfo: clientInfo, 
+          userContacts: contacts.data.filter((c) => !c.isGroup && c.isMyContact), 
+          userProfilePic: profileImage.data,
+        });
+      });
     });
+
+      socket.on("disconnect", () => {
+        setServerDisconnected(true);
+      });
   }, []);
 
   return (
     <>
       <Box className="min-h-screen">
-        {loading ? (
-          <div className="flex justify-center mt-16">
-            <CircularProgress size={40} />
-          </div>
+        {serverDisconnected ? (
+          <h1 className="text-red-600 text-center mt-12" style={{ fontSize: "38px"}}>
+            Server Disconnected!
+          </h1>
         ) : (
           <>
-            <h1>Whatsapp</h1>
-            {ready ? (
-              <div className="mt-5">
-                <h1 style={{ fontSize: "38px" }}>Connected</h1>
-                <img src={data?.userProfilePic} width={60} height={60} className="rounded" alt=""/>
-                <strong>{data?.userInfo?.pushname}</strong>
-                <p>{data?.userInfo?.me?.user}</p>
-                <div style={{ marginTop: "40px" }}>
-                  <TextField
-                    type="text"
-                    placeholder="Type here.."
-                    value={messageValue}
-                    onInput={handleInput}
-                  />
-                  <Select onChange={handleSelectContact} value={contactValue}>
-                    {data?.userContacts?.map((contact, index) => {
-                      return (
-                        <MenuItem key={`${index}${contact.number}`} value={contact.number}>
-                          {contact.name || contact.number}
-                        </MenuItem>
-                      );
-                    })}
-                  </Select>
-                  <div className="mt-2"><Button variant="contained" onClick={handleSend}>Send</Button></div>
-                </div>
+            {loading ? (
+              <div className="flex justify-center mt-16">
+                <CircularProgress size={40} />
               </div>
             ) : (
-              <img src={qr} alt="" />
+              <>
+                <h1>Whatsapp</h1>
+                {ready ? (
+                  <div className="mt-5">
+                    <h1 style={{ fontSize: "38px", fontWeight: "bold" }}>
+                      Connected
+                    </h1>
+                    <img
+                      src={data?.userProfilePic}
+                      width={60}
+                      height={60}
+                      className="rounded"
+                      alt=""
+                    />
+                    <strong>{data?.userInfo?.pushname}</strong>
+                    <p>{data?.userInfo?.me?.user}</p>
+                    <div style={{ marginTop: "40px" }}>
+                      <TextField
+                        type="text"
+                        placeholder="Type here.."
+                        value={messageValue}
+                        onInput={handleInput}
+                      />
+                      <Select
+                        onChange={handleSelectContact}
+                        value={contactValue}
+                      >
+                        <MenuItem value="0" disabled selected>
+                          Select Contact
+                        </MenuItem>
+                        {data?.userContacts?.map((contact, index) => {
+                          return (
+                            <MenuItem
+                              key={`${index}${contact.number}`}
+                              value={contact.number}
+                            >
+                              {contact.name || contact.number}
+                            </MenuItem>
+                          );
+                        })}
+                      </Select>
+                      <div className="mt-2">
+                        <Button variant="contained" onClick={handleSend}>
+                          Send
+                        </Button>
+                      </div>
+                    </div>
+
+                    <div className="mt-4">
+                      {selectedContact ? (
+                        <div>{selectedContact.number}</div>
+                      ) : (
+                        <>
+                          <h1 style={{ fontSize: "38px", fontWeight: "bold" }}>
+                            Contacts
+                          </h1>
+                          {data?.userContacts?.map((contact, index) => {
+                            return (
+                              <div
+                                onClick={() => selectContact(contact)}
+                                className="bg-slate-700 text-white py-2 px-5 rounded mb-2 cursor-pointer"
+                                key={`${index}${contact.number}`}
+                              >
+                                {contact.name || contact.number}
+                              </div>
+                            );
+                          })}
+                        </>
+                      )}
+                    </div>
+                  </div>
+                ) : (
+                  qr && <QRCodeCanvas style={{width: 150, height: 150}} value={qr} />
+                )}
+              </>
             )}
           </>
         )}
