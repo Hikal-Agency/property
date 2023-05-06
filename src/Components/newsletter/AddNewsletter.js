982996d74b65d19179d2f5de4dbad454c8ad8e95
Ubaid <malikunknown633@gmail.com>
2023-05-06 15:54:43 +0500

    Added validations in newletter

--- src/Components/newsletter/AddNewsletter.js
+++ src/Components/newsletter/AddNewsletter.js
@@ -28,17 +28,57 @@ const AddNewsletter = ({ tabValue, setTabValue, isLoading }) => {
   const [validToDate, setValidToDate] = useState("");
   const [validToDateValue, setValidToDateValue] = useState({});
   const [loading, setloading] = useState(false);
+  const [error, setError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [email, setEmail] = useState("");
 
   const [newsletterData, setNewsletterData] = useState({
     email: "",
     status: "",
   });
 
+  const handleEmail = (e) => {
+    setEmailError(false);
+    const value = e.target.value;
+    setNewsletterData({ ...newsletterData, email: value });
+
+    console.log(value);
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+    if (emailRegex.test(value)) {
+      setEmailError(false);
+      setNewsletterData({ ...newsletterData, email: value });
+    } else {
+      setEmailError("Kindly enter a valid email.");
+      return;
+    }
+    // setNewsletterData({ ...newsletterData, email: value });
+    // setEmail(value);
+    console.log("Email state: ", newsletterData?.email);
+  };
+
   const handleClick = async (e) => {
     e.preventDefault();
     setloading(true);
     if (!newsletterData?.email || !newsletterData?.status) {
       toast.error("Kindly enter all fields.", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+
+      setloading(false);
+
+      return;
+    }
+
+    if (!emailError === false) {
+      toast.error("Kindly enter a valid email.", {
         position: "top-right",
         autoClose: 3000,
         hideProgressBar: false,
@@ -61,8 +101,8 @@ const AddNewsletter = ({ tabValue, setTabValue, isLoading }) => {
     const creationDate = new Date();
     const NewsLetter = new FormData();
 
-    NewsLetter.append("email", newsletterData.email);
-    NewsLetter.append("status", newsletterData.status);
+    NewsLetter.append("email", newsletterData?.email);
+    NewsLetter.append("status", newsletterData?.status);
 
     console.log("NewsLetter append: ", NewsLetter);
 
@@ -135,6 +175,7 @@ const AddNewsletter = ({ tabValue, setTabValue, isLoading }) => {
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 2xl:grid-cols-1 gap-5 py-5">
           <Box sx={darkModeColors}>
             <TextField
+              id="email"
               type={"email"}
               label="Email "
               className="w-full mb-3"
@@ -142,10 +183,14 @@ const AddNewsletter = ({ tabValue, setTabValue, isLoading }) => {
               variant="outlined"
               name="email"
               size="medium"
-              value={newsletterData.email}
-              onChange={(e) =>
-                setNewsletterData({ ...newsletterData, email: e.target.value })
-              }
+              value={newsletterData?.email}
+              // value={email}
+              // onChange={(e) =>
+              //   setNewsletterData({ ...newsletterData, email: e.target.value })
+              // }
+              error={emailError && emailError}
+              helperText={emailError && emailError}
+              onChange={handleEmail}
             />
 
             <Select
