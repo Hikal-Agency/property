07fb4030c06d5547ac99b8687bb3eb93247dd188
Ubaid <malikunknown633@gmail.com>
2023-05-19 10:54:44 +0500

    FIX: all users with status 1 were deactive

--- src/Pages/contacts/index.js
+++ src/Pages/contacts/index.js
@@ -9,15 +9,12 @@ import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 
 const Contacts = () => {
-  const {
-    currentMode,
-    BACKEND_URL,
-  } = useStateContext();
+  const { currentMode, BACKEND_URL } = useStateContext();
   const [loading, setloading] = useState(true);
   const [contacts, setContacts] = useState([]);
   const [page, setPage] = useState("1");
   const [maxPage, setMaxPage] = useState(0);
-  const token = localStorage.getItem('auth-token');
+  const token = localStorage.getItem("auth-token");
   //eslint-disable-next-line
   const ContactData = [
     {
@@ -111,71 +108,69 @@ const Contacts = () => {
         {loading ? (
           <Loader />
         ) : (
-            <div
-              className={`w-full  ${
-                currentMode === "dark" ? "bg-black" : "bg-white"
-              }`}
-            >
-              <div className="px-5">
-                
+          <div
+            className={`w-full  ${
+              currentMode === "dark" ? "bg-black" : "bg-white"
+            }`}
+          >
+            <div className="px-5">
+              <div className="mt-5 md:mt-2">
+                <h1
+                  className={`font-semibold ${
+                    currentMode === "dark" ? "text-white" : "text-black"
+                  } text-xl ml-2 mb-3 auto-cols-max gap-x-3`}
+                >
+                  Contacts
+                  {/* <span className="px-5 py-3 rounded-md">Leaderboard</span> */}
+                  {/* <span className="px-5 py-3 rounded-md">Call Log Board</span> */}
+                </h1>
 
-                <div className="mt-5 md:mt-2">
-                  <h1
-                    className={`font-semibold ${
-                      currentMode === "dark" ? "text-white" : "text-black"
-                    } text-xl ml-2 mb-3 auto-cols-max gap-x-3`}
-                  >
-                    Contacts
-                    {/* <span className="px-5 py-3 rounded-md">Leaderboard</span> */}
-                    {/* <span className="px-5 py-3 rounded-md">Call Log Board</span> */}
-                  </h1>
-
-                  <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 pb-3">
-                    {contacts?.map((item, index) => {
-                      return (
-                        <div
-                          key={index}
-                          className={`${
-                            currentMode === "dark"
-                              ? "bg-gray-900 text-white"
-                              : "bg-gray-200 text-black"
-                          } p-3 rounded-md `}
-                        >
-                          <img
-                            src="/favicon.png"
-                            className="rounded-md cursor-pointer h-[50px] w-[50px] object-cover"
-                            alt=""
-                          />
-                          <div className="mt-2 space-y-1 overflow-hidden">
-                            <h1 className="font-bold">{item.userName}</h1>
-                            <p className="text-sm">{item.position}</p>
-                            <p className="text-sm font-semibold text-red-600">
-                              {item.userName}
-                            </p>
-                            <p className="text-sm">{item.userPhone}</p>
-                            <p className="text-sm">{item.userEmail}</p>
-                            {item?.status === 1 ? (
-                              <p className="text-sm text-red-600">Deactive</p>
-                            ) : (
-                              <p className="text-sm text-green-600">Active</p>
-                            )}
-                          </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 pb-3">
+                  {contacts?.map((item, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className={`${
+                          currentMode === "dark"
+                            ? "bg-gray-900 text-white"
+                            : "bg-gray-200 text-black"
+                        } p-3 rounded-md `}
+                      >
+                        <img
+                          src="/favicon.png"
+                          className="rounded-md cursor-pointer h-[50px] w-[50px] object-cover"
+                          alt=""
+                        />
+                        <div className="mt-2 space-y-1 overflow-hidden">
+                          <h1 className="font-bold">{item.userName}</h1>
+                          <p className="text-sm">{item.position}</p>
+                          <p className="text-sm font-semibold text-red-600">
+                            {item.userName}
+                          </p>
+                          <p className="text-sm">{item.userPhone}</p>
+                          <p className="text-sm">{item.userEmail}</p>
+                          {item?.status === 0 ? (
+                            <p className="text-sm text-red-600">Deactive</p>
+                          ) : (
+                            <p className="text-sm text-green-600">Active</p>
+                          )}
                         </div>
-                      );
-                    })}
-                  </div>
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
-
-              <Stack spacing={2} marginTop={2}>
-                <Pagination
-                  count={maxPage}
-                  color="error"
-                  onChange={handlePageChange}
-                  style={{ margin: "auto" }}
-                />
-              </Stack>
             </div>
+
+            <Stack spacing={2} marginTop={2}>
+              <Pagination
+                count={maxPage}
+                color="error"
+                onChange={handlePageChange}
+                style={{ margin: "auto" }}
+              />
+            </Stack>
+          </div>
         )}
         <Footer />
       </div>
