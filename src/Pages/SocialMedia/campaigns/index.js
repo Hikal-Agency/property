4fac36c5eceb5b7fb2fdb14c0cb9f574e6389861
Ubaid <malikunknown633@gmail.com>
2023-05-22 11:32:04 +0500

    Margin bottom in statistics page

--- src/Pages/SocialMedia/campaigns/index.js
+++ src/Pages/SocialMedia/campaigns/index.js
@@ -7,8 +7,7 @@ import Loader from "../../../Components/Loader";
 import AllCampaigns from "../../../Components/campaigns/AllCampaigns";
 
 const Campaigns = () => {
-  const {currentMode, setopenBackDrop } =
-    useStateContext();
+  const { currentMode, setopenBackDrop } = useStateContext();
   const [loading, setloading] = useState(true);
 
   const [pageState, setpageState] = useState({
@@ -31,7 +30,7 @@ const Campaigns = () => {
         <title>HIKAL CRM - Lead Notes</title>
         <meta name="description" content="Meetings - HIKAL CRM" />
       </Head> */}
-      <div className="flex min-h-screen">
+      <div className="flex min-h-screen mb-[60px]">
         {loading ? (
           <Loader />
         ) : (
@@ -40,10 +39,9 @@ const Campaigns = () => {
               currentMode === "dark" ? "bg-black" : "bg-white"
             }`}
           >
-              <div className={`w-full `}>
-                <div className="px-5">
-                  
-                  {/* <div className="mt-3">
+            <div className={`w-full `}>
+              <div className="px-5">
+                {/* <div className="mt-3">
                     <h1
                       className={`text-xl border-l-[4px] ml-1 pl-1 mb-5 font-bold ${
                         currentMode === "dark"
@@ -61,26 +59,26 @@ const Campaigns = () => {
                       setpageState={setpageState}
                     />
                   </div> */}
-                  <div className="mt-3 flex justify-between items-center">
-                    <h1
-                      className={`text-xl border-l-[4px] ml-1 pl-1 mb-5 font-bold ${
-                        currentMode === "dark"
-                          ? "text-white border-white"
-                          : "text-red-600 font-bold border-red-600"
-                      }`}
-                    >
-                      Campaigns
-                      {/* <span className="bg-main-red-color text-white px-2 py-1 rounded-sm my-auto">
+                <div className="mt-3 flex justify-between items-center">
+                  <h1
+                    className={`text-xl border-l-[4px] ml-1 pl-1 mb-5 font-bold ${
+                      currentMode === "dark"
+                        ? "text-white border-white"
+                        : "text-red-600 font-bold border-red-600"
+                    }`}
+                  >
+                    Campaigns
+                    {/* <span className="bg-main-red-color text-white px-2 py-1 rounded-sm my-auto">
                         <span>{pageState?.total}</span>
                       </span> */}
-                    </h1>
-                  </div>
-                  <AllCampaigns
-                    pageState={pageState}
-                    setpageState={setpageState}
-                  />
+                  </h1>
                 </div>
+                <AllCampaigns
+                  pageState={pageState}
+                  setpageState={setpageState}
+                />
               </div>
+            </div>
             <Footer />
           </div>
         )}
