576d2b47ef21e9c9be04de8f2f30d8f4a4d7107e
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-05-26 06:06:24 -0700

    pt on login screen

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -467,7 +467,7 @@ function App() {
       <div className="flex" style={{ width: "99vw" }}>
         {hasSidebarOrNavbar() && <Sidebarmui />}
         <div
-          className={`w-[100%] pt-20 ${
+          className={`w-[100%] ${hasSidebarOrNavbar() ? "pt-20" : "pt-0"} ${
             currentMode === "dark" ? "bg-black" : "bg-white"
           }`}
         >
