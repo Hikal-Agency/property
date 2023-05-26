21ed7b5227a22c648fcdf05419f7709ced3319e7
Ubaid <malikunknown633@gmail.com>
2023-05-26 18:21:59 +0500

    GRID view in lead notes

---
+++ src/Components/meetings/GridMeeting.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GridMeeting = () => {
+  return <div>GridMeeting</div>;
+};
+
+export default GridMeeting;
---
+++ src/Components/newsletter/GridNewsletter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GridNewsletter = () => {
+  return <div>GridNewsletter</div>;
+};
+
+export default GridNewsletter;
