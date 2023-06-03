f6d17622d4629fd397f4ecaeb5ea9887a8f1049d
Ubaid <malikunknown633@gmail.com>
2023-06-03 16:42:19 +0500

    updating cards in tables

--- src/Pages/booked/index.js
+++ src/Pages/booked/index.js
@@ -1,11 +1,7 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import Footer from "../../Components/Footer/Footer";
 import BookedDeals from "../../Components/Leads/BookedDeals";
 import Loader from "../../Components/Loader";
-import Navbar from "../../Components/Navbar/Navbar";
-import Sidebarmui from "../../Components/Sidebar/Sidebarmui";
 import { useStateContext } from "../../context/ContextProvider";
 import { MdCampaign } from "react-icons/md";
 import { FaSnapchat } from "react-icons/fa";
@@ -13,6 +9,9 @@ import { FaFacebook } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { BsSnow2 } from "react-icons/bs";
 import { Box } from "@mui/material";
+import { FaTiktok } from "react-icons/fa";
+import { RiMessage2Line } from "react-icons/ri";
+import { FaWhatsapp } from "react-icons/fa";
 
 const Booked = () => {
   const navigate = useNavigate();
@@ -48,7 +47,7 @@ const Booked = () => {
               <div className="w-full">
                 <div className=" w-full flex items-center justify-between">
                   <h1
-                    className={`text-xl border-l-[4px] ml-1 pl-1 mb-5 font-bold ${
+                    className={`text-xl border-l-[4px] ml-1 pl-1  font-bold ${
                       currentMode === "dark"
                         ? "text-white border-white"
                         : "text-red-600 font-bold border-red-600"
@@ -62,138 +61,158 @@ const Booked = () => {
                   </h1>
                   <div className="justify-self-end">
                     <div className=" px-4 py-8">
-                      <div className="grid grid-cols-1 md:grid-cols-6 lg:grid-cols-6 gap-4">
+                      <div className="grid grid-cols-1 md:grid-cols-8 lg:grid-cols-8 gap-4">
                         <Box
                           sx={{
                             padding: "10px",
-                            borderRadius: "10px",
+                            borderRadius: "7px",
                             display: "flex",
                             justifyContent: "space-between",
                             alignItems: "center",
                             fontWeight: "bold",
-                            background: `${
-                              currentMode === "dark" ? "#202020" : "#fafafa"
-                            }`,
+                            background: " #da1f26",
                             color: `${
                               currentMode === "dark" ? "#ffffff" : "#000000"
                             }`,
                             boxShadow: "0px 3px 3px rgba(0, 0, 0, 0.25)",
-                            height: "70px",
+                            height: "40px",
                             width: "70px",
                           }}
                         >
-                          <span>
-                            <FaFacebook size={22} color={"#0e82e1"} />
+                          <span
+                            style={{
+                              background: "white",
+                              borderRadius: "50%",
+                            }}
+                          >
+                            <FaFacebook
+                              size={22}
+                              color={"#0e82e1"}
+                              style={{ padding: "3px" }}
+                            />
                           </span>
                           <span>{pageState?.fbCounts}</span>
                         </Box>
                         <Box
                           sx={{
                             padding: "10px",
-                            borderRadius: "10px",
+                            borderRadius: "7px",
                             display: "flex",
                             justifyContent: "space-between",
                             alignItems: "center",
                             fontWeight: "bold",
-                            background: `${
-                              currentMode === "dark" ? "#202020" : "#fafafa"
-                            }`,
+                            background: "#da1f26",
                             color: `${
                               currentMode === "dark" ? "#ffffff" : "#000000"
                             }`,
                             boxShadow: "0px 3px 3px rgba(0, 0, 0, 0.25)",
-                            height: "70px",
+                            height: "40px",
                             width: "70px",
                             justifySelf: "end",
                           }}
                         >
-                          <span>
-                            <FaSnapchat size={22} color={"#f6d80a"} />
+                          <span
+                            style={{ background: "white", borderRadius: "50%" }}
+                          >
+                            <FaSnapchat
+                              size={22}
+                              color={"#f6d80a"}
+                              style={{ padding: "3px" }}
+                            />
                           </span>
                           <span>{pageState?.spCount}</span>
                         </Box>
                         <Box
                           sx={{
                             padding: "10px",
-                            borderRadius: "10px",
+                            borderRadius: "7px",
                             display: "flex",
                             justifyContent: "space-between",
                             alignItems: "center",
                             fontWeight: "bold",
-                            background: `${
-                              currentMode === "dark" ? "#202020" : "#fafafa"
-                            }`,
+                            background: "#da1f26",
                             color: `${
                               currentMode === "dark" ? "#ffffff" : "#000000"
                             }`,
                             boxShadow: "0px 3px 3px rgba(0, 0, 0, 0.25)",
-                            height: "70px",
+                            height: "40px",
                             width: "70px",
                             justifySelf: "end",
                           }}
                         >
-                          <span>
-                            <img
+                          <span
+                            style={{ background: "white", borderRadius: "50%" }}
+                          >
+                            <FaTiktok
+                              size={22}
+                              color={"#f6d80a"}
+                              style={{ padding: "3px" }}
+                            />
+
+                            {/* <img
                               src={"/assets/tiktok-app.svg"}
                               alt=""
                               height={22}
                               width={22}
                               className="object-cover"
-                            />
+                            /> */}
                           </span>
                           <span>{pageState?.ttCount}</span>
                         </Box>
                         <Box
                           sx={{
                             padding: "10px",
-                            borderRadius: "10px",
+                            borderRadius: "7px",
                             display: "flex",
                             justifyContent: "space-between",
                             alignItems: "center",
                             fontWeight: "bold",
-                            background: `${
-                              currentMode === "dark" ? "#202020" : "#fafafa"
-                            }`,
+                            background: "#da1f26",
                             color: `${
                               currentMode === "dark" ? "#ffffff" : "#000000"
                             }`,
                             boxShadow: "0px 3px 3px rgba(0, 0, 0, 0.25)",
-                            height: "70px",
+                            height: "40px",
                             width: "70px",
                             justifySelf: "end",
                           }}
                         >
-                          <span>
-                            <FcGoogle size={22} />
+                          <span
+                            style={{ background: "white", borderRadius: "50%" }}
+                          >
+                            <FcGoogle
+                              size={22}
+                              color={"#0e82e1"}
+                              style={{ padding: "3px" }}
+                            />
                           </span>
                           <span>{pageState?.gCount}</span>
                         </Box>
                         <Box
                           sx={{
                             padding: "10px",
-                            borderRadius: "10px",
+                            borderRadius: "7px",
                             display: "flex",
                             justifyContent: "space-between",
                             alignItems: "center",
                             fontWeight: "bold",
-                            background: `${
-                              currentMode === "dark" ? "#202020" : "#fafafa"
-                            }`,
+                            background: "#da1f26",
                             color: `${
                               currentMode === "dark" ? "#ffffff" : "#000000"
                             }`,
                             boxShadow: "0px 3px 3px rgba(0, 0, 0, 0.25)",
-                            height: "70px",
+                            height: "40px",
                             width: "70px",
                             justifySelf: "end",
                           }}
                         >
-                          <span>
+                          <span
+                            style={{ background: "white", borderRadius: "50%" }}
+                          >
                             <MdCampaign
                               size={22}
-                              color={`${
-                                currentMode === "dark" ? "#ffffff" : "#000000"
-                              }`}
+                              color={"#0e82e1"}
+                              style={{ padding: "3px" }}
                             />
                           </span>
                           <span>{pageState?.cCount}</span>
@@ -201,27 +220,89 @@ const Booked = () => {
                         <Box
                           sx={{
                             padding: "10px",
-                            borderRadius: "10px",
+                            borderRadius: "7px",
                             display: "flex",
                             justifyContent: "space-between",
                             alignItems: "center",
                             fontWeight: "bold",
-                            background: `${
-                              currentMode === "dark" ? "#202020" : "#fafafa"
-                            }`,
+                            background: "#da1f26",
                             color: `${
                               currentMode === "dark" ? "#ffffff" : "#000000"
                             }`,
                             boxShadow: "0px 3px 3px rgba(0, 0, 0, 0.25)",
                             justifySelf: "end",
-                            height: "70px",
+                            height: "40px",
                             width: "70px",
                           }}
                         >
-                          <span>
-                            <BsSnow2 size={22} color={"#0ec7ff"} />
+                          <span
+                            style={{ background: "white", borderRadius: "50%" }}
+                          >
+                            <BsSnow2
+                              size={22}
+                              color={"#0e82e1"}
+                              style={{ padding: "3px" }}
+                            />
                           </span>
                           <span>{pageState?.coCount}</span>
+                        </Box>
+                        <Box
+                          sx={{
+                            padding: "10px",
+                            borderRadius: "7px",
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                            fontWeight: "bold",
+                            background: "#da1f26",
+                            color: `${
+                              currentMode === "dark" ? "#ffffff" : "#000000"
+                            }`,
+                            boxShadow: "0px 3px 3px rgba(0, 0, 0, 0.25)",
+                            justifySelf: "end",
+                            height: "40px",
+                            width: "70px",
+                          }}
+                        >
+                          <span
+                            style={{ background: "white", borderRadius: "50%" }}
+                          >
+                            <RiMessage2Line
+                              size={22}
+                              color={"#0e82e1"}
+                              style={{ padding: "3px" }}
+                            />
+                          </span>
+                          <span>{pageState?.mCount}</span>
+                        </Box>
+                        <Box
+                          sx={{
+                            padding: "10px",
+                            borderRadius: "7px",
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                            fontWeight: "bold",
+                            background: "#da1f26",
+                            color: `${
+                              currentMode === "dark" ? "#ffffff" : "#000000"
+                            }`,
+                            boxShadow: "0px 3px 3px rgba(0, 0, 0, 0.25)",
+                            justifySelf: "end",
+                            height: "40px",
+                            width: "70px",
+                          }}
+                        >
+                          <span
+                            style={{ background: "white", borderRadius: "50%" }}
+                          >
+                            <FaWhatsapp
+                              size={22}
+                              color={"#0e82e1"}
+                              style={{ padding: "3px" }}
+                            />
+                          </span>
+                          <span>{pageState?.wCount}</span>
                         </Box>
                       </div>
                     </div>
