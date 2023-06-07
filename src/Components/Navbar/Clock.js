64ae3f6577b17d6f8bd30390abbdf30a77aa588e
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-06-07 06:13:57 -0700

    timezone added on navbar

--- src/Components/Navbar/Clock.js
+++ src/Components/Navbar/Clock.js
@@ -3,9 +3,9 @@ import moment from 'moment-timezone';
 import { MenuItem, Select } from '@mui/material';
 
 const Clock = () => {
-  const [currentTime, setCurrentTime] = useState(moment());
+  const [currentTime, setCurrentTime] = useState(moment().tz(moment.tz.guess()).format('D/MM/YYYY, h:mm:ss a [GMT]Z'));
   const [timezones, setTimezones] = useState([]);
-  const [selectedTimezone, setSelectedTimezone] = useState('');
+  const [selectedTimezone, setSelectedTimezone] = useState(moment.tz.guess());
 
   useEffect(() => {
     // Fetch all timezones
@@ -13,17 +13,18 @@ const Clock = () => {
     setTimezones(fetchedTimezones);
 
     // Set default timezone
-    setSelectedTimezone(moment.tz.guess());
+    // setSelectedTimezone(moment.tz.guess());
 
     // Update current time every second
     const interval = setInterval(() => {
-      setCurrentTime(moment());
+      console.log(selectedTimezone)
+      setCurrentTime(moment().tz(selectedTimezone).format('D/MM/YYYY, h:mm:ss a [GMT]Z'));
     }, 1000);
 
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [selectedTimezone]);
 
   const handleTimezoneChange = (e) => {
     setSelectedTimezone(e.target.value);
@@ -32,11 +33,11 @@ const Clock = () => {
   return (
     <div>
       <div className="flex items-center mr-2">
-        <h2 style={{marginRight: 8, fontSize: 14}}>{currentTime.format('D/MM/YYYY, h:mm:ss a [GMT]Z')}</h2>
+        <h2 style={{marginRight: 8, fontSize: 14}}>{currentTime}</h2>
          <Select className='time-zone-select' sx={{padding: 0, "& .MuiSelect-select": {
             padding: "0 25px 0 5px !important", 
          }}} size="small" value={selectedTimezone} onChange={handleTimezoneChange}>
-          {timezones.map((timezone) => (
+          {timezones?.map((timezone) => (
             <MenuItem key={timezone} value={timezone}>
               {timezone}
             </MenuItem>
