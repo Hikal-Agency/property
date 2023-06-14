09d2111fda3a580727cd434a4ae1f987531ac387
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-06-14 05:51:38 -0700

    templates page redesign

--- src/Components/whatsapp-marketing/TemplatesCountCard.js
+++ src/Components/whatsapp-marketing/TemplatesCountCard.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 const TemplatesCountCard = ({count, type, icon})=> {
     const {currentMode } = useStateContext();
     return (<>
-            <Box className={`relative p-3 w-[32%] rounded shadow-lg border ${currentMode === "dark" ? 'border-red-600' : 'border-gray-500'} flex flex-col justify-between`}>
+            <Box style={{background: currentMode === "dark" ? "#111827" : "#da1f26"}} className={`relative text-white p-3 w-[32%] rounded shadow-lg border ${currentMode === "dark" ? 'border-red-600' : 'border-gray-500'} flex flex-col justify-between`}>
                 <h4 className="mb-4 font-bold" style={{fontSize: 22}}>{count}</h4>
                 <p>{type}</p>
                 <Box sx={{position: "absolute", right: 12, top: 12}}>
