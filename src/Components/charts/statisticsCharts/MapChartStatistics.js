0f11c7a895e9e75fdb2a2f90a3b0ee11d3b8f94e
ubaidawan <malikunknown633@gmail.com>
2023-06-15 10:03:47 +0500

    statistics

--- src/Components/charts/statisticsCharts/MapChartStatistics.js
+++ src/Components/charts/statisticsCharts/MapChartStatistics.js
@@ -33,60 +33,12 @@ import "./styling.css";
 //   return <div id="chartdiv" style={{ width: "100%" }} className="h-full" />;
 // };
 
-const MapChartStatistics = ({ locationData }) => {
-  console.log("location map: ", locationData);
-
-  useEffect(() => {
-    // Create root and chart
-    let root = am5.Root.new("chartdiv");
-    let chart = root.container.children.push(
-      am5map.MapChart.new(root, {
-        panX: "rotateX",
-        projection: am5map.geoNaturalEarth1(),
-      })
-    );
-
-    // Create polygon series
-    let polygonSeries = chart.series.push(
-      am5map.MapPolygonSeries.new(root, {
-        geoJSON: am5geodata_worldLow,
-        exclude: ["AQ"],
-      })
-    );
-
-    // Create point series for markers
-    let pointSeries = chart.series.push(am5map.MapPointSeries.new(root, {}));
-
-    pointSeries.events.on("ready", function () {
-      let pointTemplate = pointSeries.mapPoints.template;
-
-      // Creating a marker on the map
-      pointTemplate.propertyFields.latitude = "latitude";
-      pointTemplate.propertyFields.longitude = "longitude";
-
-      // Adding data to pointSeries
-      let markerData = locationData?.geo_locations?.places?.map((place) => ({
-        latitude: place.latitude,
-        longitude: place.longitude,
-      }));
-
-      pointSeries.data.setAll(markerData);
-    });
-
-    // Clean up the chart when the component is unmounted
-    return () => {
-      chart.dispose();
-      root.dispose();
-    };
-  }, [locationData]); // Make sure to add 'locationData' in the dependency array
-
-  return <div id="chartdiv" style={{ width: "100%" }} className="h-full" />;
-};
-
 // const MapChartStatistics = ({ locationData }) => {
-//   useEffect(() => {
-//     let root = am5.Root.new("chartdiv");
+//   console.log("location map: ", locationData);
 
+//   useEffect(() => {
+//     // Create root and chart
+//     let root = am5.Root.new("chartdiv");
 //     let chart = root.container.children.push(
 //       am5map.MapChart.new(root, {
 //         panX: "rotateX",
@@ -94,6 +46,7 @@ const MapChartStatistics = ({ locationData }) => {
 //       })
 //     );
 
+//     // Create polygon series
 //     let polygonSeries = chart.series.push(
 //       am5map.MapPolygonSeries.new(root, {
 //         geoJSON: am5geodata_worldLow,
@@ -101,34 +54,77 @@ const MapChartStatistics = ({ locationData }) => {
 //       })
 //     );
 
+//     // Create point series for markers
 //     let pointSeries = chart.series.push(am5map.MapPointSeries.new(root, {}));
 
-//     let circleTemplate = am5.Template.new({
-//       radius: 5,
-//       fill: root.interfaceColors.get("primaryButtonBackground"),
-//       strokeWidth: 2,
-//       stroke: root.interfaceColors.get("primaryButtonStroke"),
-//     });
-//     pointSeries.bullets.template.setAll(circleTemplate);
+//     pointSeries.events.on("ready", function () {
+//       let pointTemplate = pointSeries.mapPoints.template;
 
-//     let markerData = locationData?.flatMap((item) =>
-//       item.geo_locations?.places?.map((place) => ({
+//       // Creating a marker on the map
+//       pointTemplate.propertyFields.latitude = "latitude";
+//       pointTemplate.propertyFields.longitude = "longitude";
+
+//       // Adding data to pointSeries
+//       let markerData = locationData?.geo_locations?.places?.map((place) => ({
 //         latitude: place.latitude,
 //         longitude: place.longitude,
-//       }))
-//     );
+//       }));
 
-//     if (markerData && markerData.length > 0) {
 //       pointSeries.data.setAll(markerData);
-//     }
+//     });
 
+//     // Clean up the chart when the component is unmounted
 //     return () => {
-//       root.dispose();
 //       chart.dispose();
+//       root.dispose();
 //     };
-//   }, [locationData]);
+//   }, [locationData]); // Make sure to add 'locationData' in the dependency array
 
-//   return <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>;
+//   return <div id="chartdiv" style={{ width: "100%" }} className="h-full" />;
 // };
+
+const MapChartStatistics = ({ locationData }) => {
+  return (
+    <div className="w-full" style={{ maxHeight: "300px", overflow: "auto" }}>
+      <table className="w-full">
+        <thead>
+          <tr>
+            <th className="border-b border-gray-300 py-2">ID</th>
+            <th className="border-b border-gray-300 py-2">Country</th>
+            <th className="border-b border-gray-300 py-2">Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {locationData?.length > 0 ? (
+            locationData?.flatMap((item, index) =>
+              item.geo_locations.places.map((place, placeIndex) => (
+                <tr key={`${index}-${placeIndex}`}>
+                  <td className="border-b border-gray-300 py-2 text-center">
+                    {placeIndex}
+                  </td>
+                  <td className="border-b border-gray-300 py-2 text-center">
+                    {place.country ? `${place.country}` : "No data"}
+                  </td>
+                  <td className="border-b border-gray-300 py-2 text-center">
+                    {place.name ? `${place.name}` : "No data"}
+                  </td>
+                </tr>
+              ))
+            )
+          ) : (
+            <tr>
+              <td
+                className="border-b border-gray-300 py-2 text-center"
+                colSpan="3"
+              >
+                No data available
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default MapChartStatistics;
