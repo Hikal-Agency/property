ace6f18b6431071a560203a05feb4dd518fab3cb
ubaidawan <malikunknown633@gmail.com>
2023-07-05 11:09:14 +0500

    loader in single lead model

--- src/Components/Leads/SingleLead.js
+++ src/Components/Leads/SingleLead.js
@@ -35,6 +35,7 @@ const SingleLead = ({
   const [AddNoteTxt, setAddNoteTxt] = useState("");
   const [addNoteloading, setaddNoteloading] = useState(false);
   const [lastNote, setLastNote] = useState("");
+  const [loading, setLoading] = useState(false);
 
   console.log("leaddata: ", LeadData);
 
@@ -44,6 +45,7 @@ const SingleLead = ({
   };
 
   const FetchLead = async () => {
+    setLoading(true);
     try {
       const token = localStorage.getItem("auth-token");
       const result = await axios.get(`${BACKEND_URL}/leads/${LeadData}`, {
@@ -56,7 +58,9 @@ const SingleLead = ({
       console.log("leads: ", result);
 
       setLeadData(result?.data?.data);
+      setLoading(false);
     } catch (error) {
+      setLoading(false);
       console.log(error);
     }
   };
@@ -195,179 +199,187 @@ const SingleLead = ({
           >
             Lead details
           </h1>
-          <div className="grid grid-cols-5 md:grid-cols-5 sm:grid-cols-1 gap-5">
-            <div className="col-span-3 space-y-2">
-              <div className="flex items-center justify-between">
-                <div className="flex space-x-2">
-                  <h6
-                    className={`font-bold ${
-                      currentMode === "dark" ? "text-white" : "text-black"
-                    }`}
-                  >
-                    Lead Name :
-                  </h6>
-                  <h6
-                    className={`font-semibold ${
-                      currentMode === "dark" ? "text-white" : "text-black"
-                    }`}
-                  >
-                    {LeadData?.leadName}
-                  </h6>
-                </div>
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex space-x-2">
-                  <h6
-                    className={`font-bold ${
-                      currentMode === "dark" ? "text-white" : "text-black"
-                    }`}
-                  >
-                    Contact Details:
-                  </h6>
-                  <h6
-                    className={`font-semibold ${
-                      currentMode === "dark" ? "text-white" : "text-black"
-                    }`}
-                  >
-                    {LeadData?.leadContact}
-                  </h6>
-                  <h6
-                    className={`font-semibold ${
-                      currentMode === "dark" ? "text-white" : "text-black"
-                    }`}
-                  >
-                    {LeadData?.LeadEmail}
-                  </h6>
-                </div>
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex space-x-2">
-                  <h6
-                    className={`font-bold ${
-                      currentMode === "dark" ? "text-white" : "text-black"
-                    }`}
-                  >
-                    Preferred language:
-                  </h6>
-                  <h6
-                    className={`font-semibold ${
-                      currentMode === "dark" ? "text-white" : "text-black"
-                    }`}
-                  >
-                    {LeadData?.language}
-                  </h6>
-                </div>
-              </div>
+          {loading ? (
+            <div className="flex justify-center">
+              <CircularProgress />
             </div>
-            <div className="col-span-2 space-y-2 text-right">
-              <div className="mb-5 space-x-3">
-                <span className="p-2 bg-main-red-color text-white rounded-md">
-                  {LeadData?.feedback ?? "No Feedback"}
-                </span>
+          ) : (
+            <>
+              <div className="grid grid-cols-5 md:grid-cols-5 sm:grid-cols-1 gap-5">
+                <div className="col-span-3 space-y-2">
+                  <div className="flex items-center justify-between">
+                    <div className="flex space-x-2">
+                      <h6
+                        className={`font-bold ${
+                          currentMode === "dark" ? "text-white" : "text-black"
+                        }`}
+                      >
+                        Lead Name :
+                      </h6>
+                      <h6
+                        className={`font-semibold ${
+                          currentMode === "dark" ? "text-white" : "text-black"
+                        }`}
+                      >
+                        {LeadData?.leadName}
+                      </h6>
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <div className="flex space-x-2">
+                      <h6
+                        className={`font-bold ${
+                          currentMode === "dark" ? "text-white" : "text-black"
+                        }`}
+                      >
+                        Contact Details:
+                      </h6>
+                      <h6
+                        className={`font-semibold ${
+                          currentMode === "dark" ? "text-white" : "text-black"
+                        }`}
+                      >
+                        {LeadData?.leadContact}
+                      </h6>
+                      <h6
+                        className={`font-semibold ${
+                          currentMode === "dark" ? "text-white" : "text-black"
+                        }`}
+                      >
+                        {LeadData?.LeadEmail}
+                      </h6>
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <div className="flex space-x-2">
+                      <h6
+                        className={`font-bold ${
+                          currentMode === "dark" ? "text-white" : "text-black"
+                        }`}
+                      >
+                        Preferred language:
+                      </h6>
+                      <h6
+                        className={`font-semibold ${
+                          currentMode === "dark" ? "text-white" : "text-black"
+                        }`}
+                      >
+                        {LeadData?.language}
+                      </h6>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-span-2 space-y-2 text-right">
+                  <div className="mb-5 space-x-3">
+                    <span className="p-2 bg-main-red-color text-white rounded-md">
+                      {LeadData?.feedback ?? "No Feedback"}
+                    </span>
+                  </div>
+                  <p
+                    className={`italic text-sm ${
+                      currentMode === "dark" ? "text-white" : "text-black"
+                    }`}
+                  >
+                    Lead added on {LeadData?.creationDate}
+                  </p>
+                  <p
+                    className={`italic text-sm ${
+                      currentMode === "dark" ? "text-white" : "text-black"
+                    }`}
+                  >
+                    Lead edited on{" "}
+                    {LeadData?.lastEdited === "" ? "-" : LeadData?.lastEdited}
+                  </p>
+                </div>
               </div>
-              <p
-                className={`italic text-sm ${
+              <div className="bg-main-red-color h-0.5 w-full my-7"></div>
+
+              <div
+                className={`${
                   currentMode === "dark" ? "text-white" : "text-black"
-                }`}
+                } grid grid-cols-4 md:grid-cols-4 sm:grid-cols-2 gap-5 `}
               >
-                Lead added on {LeadData?.creationDate}
-              </p>
-              <p
-                className={`italic text-sm ${
-                  currentMode === "dark" ? "text-white" : "text-black"
-                }`}
-              >
-                Lead edited on{" "}
-                {LeadData?.lastEdited === "" ? "-" : LeadData?.lastEdited}
-              </p>
-            </div>
-          </div>
-          <div className="bg-main-red-color h-0.5 w-full my-7"></div>
+                <div className="grid justify-center space-y-3 text-center">
+                  <h6 className="font-bold">Project</h6>
+                  <h6 className="font-semibold">{LeadData?.project}</h6>
+                </div>
+                <div className="grid justify-center space-y-3 text-center">
+                  <h6 className="font-bold">How many bedrooms?</h6>
+                  <h6 className="font-semibold">{LeadData?.enquiryType}</h6>
+                </div>
+                <div className="grid justify-center space-y-3 text-center">
+                  <h6 className="font-bold">Property type</h6>
+                  <h6 className="font-semibold">{LeadData?.leadType}</h6>
+                </div>
+                <div className="grid justify-center space-y-3 text-center">
+                  <h6 className="font-bold">Purpose</h6>
+                  <h6 className="font-semibold">{LeadData?.leadFor}</h6>
+                </div>
+              </div>
 
-          <div
-            className={`${
-              currentMode === "dark" ? "text-white" : "text-black"
-            } grid grid-cols-4 md:grid-cols-4 sm:grid-cols-2 gap-5 `}
-          >
-            <div className="grid justify-center space-y-3 text-center">
-              <h6 className="font-bold">Project</h6>
-              <h6 className="font-semibold">{LeadData?.project}</h6>
-            </div>
-            <div className="grid justify-center space-y-3 text-center">
-              <h6 className="font-bold">How many bedrooms?</h6>
-              <h6 className="font-semibold">{LeadData?.enquiryType}</h6>
-            </div>
-            <div className="grid justify-center space-y-3 text-center">
-              <h6 className="font-bold">Property type</h6>
-              <h6 className="font-semibold">{LeadData?.leadType}</h6>
-            </div>
-            <div className="grid justify-center space-y-3 text-center">
-              <h6 className="font-bold">Purpose</h6>
-              <h6 className="font-semibold">{LeadData?.leadFor}</h6>
-            </div>
-          </div>
-
-          <div className="bg-main-red-color h-0.5 w-full mt-6 mb-4"></div>
-          <div className="flex my-0 w-full">
-            <Link
-              sx={{ my: 0, w: "100%" }}
-              to={`/lead/${LeadData?.leadId || LeadData?.lid}`}
-              target="_blank"
-            >
-              <Button
-                fullWidth
-                sx={{ my: 0 }}
-                variant="contained"
-                color="error"
-                size="medium"
-              >
-                View Lead Details
-              </Button>
-            </Link>
-          </div>
-          <div className={`rounded-md mt-2`}>
-            {lastNote && (
-              <Box className="bg-gray-300 rounded px-2 py-1 mt-3">
-                {lastNote}
-              </Box>
-            )}
-            <form
-              className="mt-5"
-              onSubmit={(e) => {
-                e.preventDefault();
-                AddNote();
-              }}
-            >
-              <TextField
-                sx={darkModeColors}
-                id="note"
-                type={"text"}
-                label="Your Note"
-                className="w-full"
-                variant="outlined"
-                size="small"
-                required
-                value={AddNoteTxt}
-                onChange={(e) => setAddNoteTxt(e.target.value)}
-              />
-
-              <button
-                disabled={addNoteloading ? true : false}
-                type="submit"
-                className="mt-3 disabled:opacity-50 disabled:cursor-not-allowed group relative flex w-full justify-center rounded-md border border-transparent bg-main-red-color p-1 text-white hover:bg-main-red-color-2 focus:outline-none focus:ring-2 focus:ring-main-red-color-2 focus:ring-offset-2 text-md font-bold uppercase"
-              >
-                {addNoteloading ? (
-                  <CircularProgress
-                    sx={{ color: "white" }}
-                    size={25}
-                    className="text-white"
-                  />
-                ) : (
-                  <span>Add Note</span>
+              <div className="bg-main-red-color h-0.5 w-full mt-6 mb-4"></div>
+              <div className="flex my-0 w-full">
+                <Link
+                  sx={{ my: 0, w: "100%" }}
+                  to={`/lead/${LeadData?.leadId || LeadData?.lid}`}
+                  target="_blank"
+                >
+                  <Button
+                    fullWidth
+                    sx={{ my: 0 }}
+                    variant="contained"
+                    color="error"
+                    size="medium"
+                  >
+                    View Lead Details
+                  </Button>
+                </Link>
+              </div>
+              <div className={`rounded-md mt-2`}>
+                {lastNote && (
+                  <Box className="bg-gray-300 rounded px-2 py-1 mt-3">
+                    {lastNote}
+                  </Box>
                 )}
-              </button>
-            </form>
-          </div>
+                <form
+                  className="mt-5"
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    AddNote();
+                  }}
+                >
+                  <TextField
+                    sx={darkModeColors}
+                    id="note"
+                    type={"text"}
+                    label="Your Note"
+                    className="w-full"
+                    variant="outlined"
+                    size="small"
+                    required
+                    value={AddNoteTxt}
+                    onChange={(e) => setAddNoteTxt(e.target.value)}
+                  />
+
+                  <button
+                    disabled={addNoteloading ? true : false}
+                    type="submit"
+                    className="mt-3 disabled:opacity-50 disabled:cursor-not-allowed group relative flex w-full justify-center rounded-md border border-transparent bg-main-red-color p-1 text-white hover:bg-main-red-color-2 focus:outline-none focus:ring-2 focus:ring-main-red-color-2 focus:ring-offset-2 text-md font-bold uppercase"
+                  >
+                    {addNoteloading ? (
+                      <CircularProgress
+                        sx={{ color: "white" }}
+                        size={25}
+                        className="text-white"
+                      />
+                    ) : (
+                      <span>Add Note</span>
+                    )}
+                  </button>
+                </form>
+              </div>
+            </>
+          )}
         </div>
       </Modal>
     </>
