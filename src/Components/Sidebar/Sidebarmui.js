3f965ec1243251270470625ac314b794f9d2532a
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-07-07 15:41:25 +0500

    sub menu on unassigned leads removed from sidebar

--- src/Components/Sidebar/Sidebarmui.js
+++ src/Components/Sidebar/Sidebarmui.js
@@ -1107,49 +1107,51 @@ const Sidebarmui = () => {
           submenu: [
             {
               name: "Fresh leads",
-              icon: <SiHotjar />,
-              submenu: [
-                {
-                  name: "All",
-                  count: sidebarData?.UNASSIGNED?.fresh,
-                  link: "/unassigned/fresh",
-                },
-                {
-                  name: "New",
-                  count: sidebarData?.HotLeadsCount?.new,
-                  link: "/unassigned/new",
-                },
-                {
-                  name: "No Answer",
-                  count: sidebarData?.HotLeadsCount?.no_nswer,
-                  link: "/unassigned/no answer",
-                },
-                {
-                  name: "Meeting",
-                  count: sidebarData?.HotLeadsCount?.Meeting,
-                  link: "/unassigned/meeting",
-                },
-                {
-                  name: "Follow Up",
-                  count: sidebarData?.HotLeadsCount?.follow_up,
-                  link: "/unassigned/follow up",
-                },
-                {
-                  name: "Low Budget",
-                  count: sidebarData?.HotLeadsCount?.low_budget,
-                  link: "/unassigned/low budget",
-                },
-                {
-                  name: "Not Interested",
-                  count: sidebarData?.HotLeadsCount?.not_interested,
-                  link: "/unassigned/not interested",
-                },
-                {
-                  name: "Unreachable",
-                  count: sidebarData?.HotLeadsCount?.unreachable,
-                  link: "/unassigned/unreachable",
-                },
-              ],
+              link: "/unassigned/fresh",
+              // icon: <SiHotjar />,
+              count: sidebarData?.UNASSIGNED?.fresh
+              // submenu: [
+              //   {
+              //     name: "All",
+              //     count: sidebarData?.UNASSIGNED?.fresh,
+              //     link: "/unassigned/fresh",
+              //   },
+              //   {
+              //     name: "New",
+              //     count: sidebarData?.HotLeadsCount?.new,
+              //     link: "/unassigned/new",
+              //   },
+              //   {
+              //     name: "No Answer",
+              //     count: sidebarData?.HotLeadsCount?.no_nswer,
+              //     link: "/unassigned/no answer",
+              //   },
+              //   {
+              //     name: "Meeting",
+              //     count: sidebarData?.HotLeadsCount?.Meeting,
+              //     link: "/unassigned/meeting",
+              //   },
+              //   {
+              //     name: "Follow Up",
+              //     count: sidebarData?.HotLeadsCount?.follow_up,
+              //     link: "/unassigned/follow up",
+              //   },
+              //   {
+              //     name: "Low Budget",
+              //     count: sidebarData?.HotLeadsCount?.low_budget,
+              //     link: "/unassigned/low budget",
+              //   },
+              //   {
+              //     name: "Not Interested",
+              //     count: sidebarData?.HotLeadsCount?.not_interested,
+              //     link: "/unassigned/not interested",
+              //   },
+              //   {
+              //     name: "Unreachable",
+              //     count: sidebarData?.HotLeadsCount?.unreachable,
+              //     link: "/unassigned/unreachable",
+              //   },
+              // ],
             },
             {
               name: "Cold leads",
@@ -2395,7 +2397,7 @@ const Sidebarmui = () => {
                                   }
                                 >
                                   {link.submenu.map((menu, index) => {
-                                    if (menu?.submenu) {
+                                    {/* if (menu?.submenu) {
                                       return (
                                         <Box
                                           onClick={(e) => {
@@ -2557,7 +2559,7 @@ const Sidebarmui = () => {
                                           </SubMenu>
                                         </Box>
                                       );
-                                    }
+                                    } */}
                                     return (
                                       <Link key={index} to={`${menu.link}`}>
                                         <Box
