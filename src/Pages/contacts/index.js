806b11c0718f94d9da983de5e1a008962a269de3
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-07-11 18:38:05 +0500

    contacts page

--- src/Pages/contacts/index.js
+++ src/Pages/contacts/index.js
@@ -6,14 +6,38 @@ import { useStateContext } from "../../context/ContextProvider";
 // import axios from "axios";
 import axios from "../../axoisConfig";
 import { ToastContainer, toast } from "react-toastify";
+import {
+  Checkbox,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  MenuItem,
+  TextField,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import { Select } from "@mui/base";
 
 const Contacts = () => {
-  const { currentMode, BACKEND_URL } = useStateContext();
+  const { currentMode, BACKEND_URL, Managers, SalesPerson } = useStateContext();
   const [loading, setloading] = useState(true);
   const [contacts, setContacts] = useState([]);
   const [page, setPage] = useState(1);
   const [maxPage, setMaxPage] = useState(0);
-  const token = localStorage.getItem("auth-token");
+  const [selectedPhoneNoVal, setSelectedPhoneNoVal] = useState("");
+  const [selectedEmailVal, setSelectedEmailVal] = useState("");
+  const [phoneStatusSelected, setPhoneStatusSelected] = useState("0");
+  const [phoneTypeSelected, setPhoneTypeSelected] = useState("0");
+  const [Manager, setManager] = useState("0");
+  const [SalesPerson2, setSalesPerson2] = useState("0");
+
+  const handleRadioChange = (event, input) => {
+    if (input === "phone") {
+      setSelectedPhoneNoVal(event.target.value);
+    } else if (input === "email") {
+      setSelectedEmailVal(event.target.value);
+    }
+  };
+
   //eslint-disable-next-line
   const ContactData = [
     {
@@ -124,51 +148,223 @@ const Contacts = () => {
                   {/* <span className="px-5 py-3 rounded-md">Call Log Board</span> */}
                 </h1>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 pb-3">
-                  {contacts?.map((item, index) => {
-                    return (
-                      <div
-                        key={index}
-                        className={`${
-                          currentMode === "dark"
-                            ? "bg-gray-900 text-white"
-                            : "bg-gray-200 text-black"
-                        } p-3 rounded-md `}
-                      >
-                        {item?.profile_picture ? (
-                          <img
-                            src={`${item?.profile_picture}`}
-                            className="rounded-md cursor-pointer h-[50px] w-[50px] object-cover"
-                            alt=""
-                          />
-                        ) : (
-                          <img
-                            src="/favicon.png"
-                            className="rounded-md cursor-pointer h-[50px] w-[50px] object-cover"
-                            alt=""
-                          />
-                        )}
-                        <div className="mt-2 space-y-1 overflow-hidden">
-                          <h1 className="font-bold capitalize">
-                            {item.userName}
-                          </h1>
-                          <p className="text-sm capitalize">{item.position}</p>
-                          {/* <p className="text-sm font-semibold text-red-600">
-                            {item.userName}
-                          </p> */}
-                          <p className="text-sm">{item.userPhone}</p>
-                          <p className="text-sm">{item.userEmail}</p>
-                          <p className="text-sm">{item.userContact}</p>
-                          {item?.status === 0 ? (
-                            <p className="text-sm text-red-600">Deactive</p>
-                          ) : (
-                            <p className="text-sm text-green-600">Active</p>
-                          )}
-                        </div>
+                <Box
+                  sx={{
+                    "& .MuiFormGroup-root": {
+                      flexDirection: "row !important",
+                    },
+                    "& .MuiFormControlLabel-label": {
+                      color: "grey",
+                      fontSize: 15,
+                    },
+                    "& .filter-label": {
+                      width: 120,
+                    },
+                    "& > div > .flex": {
+                      borderBottom: "1px solid #80808024",
+                      marginBottom: "10px",
+                      paddingBottom: "6px",
+                    },
+                  }}
+                  className="flex"
+                >
+                  <div className="w-[30%] h-full p-3 border rounded mr-3">
+                    <strong className="mb-3 text-red-600">Filters</strong>
+                    <div className="flex mt-4 items-center">
+
+                      <div class="flex items-center">
+                        <div className="h-[12px] bg-[#da1f26a3] w-[12px] mr-2">
+                          </div>
+                        <p className="filter-label">Phone No</p>
                       </div>
-                    );
-                  })}
-                </div>
+                      <RadioGroup
+                        className="flex items-center ml-4"
+                        value={selectedPhoneNoVal}
+                        onChange={(e) => handleRadioChange(e, "phone")}
+                      >
+                        <FormControlLabel
+                          value="with"
+                          control={<Radio size="16" />}
+                          label="With"
+                        />
+                        <FormControlLabel
+                          value="without"
+                          control={<Radio size="16" />}
+                          label="Without"
+                        />
+                      </RadioGroup>
+                    </div>
+                    <div className="flex items-center">
+                      
+                      <div class="flex items-center">
+                        <div className="h-[12px] bg-[#da1f26a3] w-[12px] mr-2">
+                          </div>
+                        <p className="filter-label">Email Address</p>
+                      </div>
+                      <RadioGroup
+                        className="flex items-center ml-4"
+                        value={selectedEmailVal}
+                        onChange={(e) => handleRadioChange(e, "email")}
+                      >
+                        <FormControlLabel
+                          value="with"
+                          control={<Radio size="16" />}
+                          label="With"
+                        />
+                        <FormControlLabel
+                          value="without"
+                          control={<Radio size="16" />}
+                          label="Without"
+                        />
+                      </RadioGroup>
+                    </div>
+                    <div class="flex items-center">
+                      
+                      <div class="flex items-center">
+                        <div className="h-[12px] bg-[#da1f26a3] w-[12px] mr-2">
+                          </div>
+                        <p className="filter-label">Phone Status</p>
+                      </div>
+                      <TextField
+                        select
+                        id="phoneStatus"
+                        size="small"
+                        onChange={(e) => setPhoneStatusSelected(e.target.value)}
+                        value={phoneStatusSelected}
+                        displayEmpty
+                        fullWidth
+                      >
+                        <MenuItem selected value="0">
+                          Select Status
+                        </MenuItem>
+                        <MenuItem value="valid">Valid</MenuItem>
+                        <MenuItem value="invalid">Invalid</MenuItem>
+                      </TextField>
+                    </div>
+                    <div class="flex items-center">
+                      
+                      <div class="flex items-center">
+                        <div className="h-[12px] bg-[#da1f26a3] w-[12px] mr-2">
+                          </div>
+                        <p className="filter-label">Phone Type</p>
+                      </div>
+                      <TextField
+                        select
+                        id="phoneType"
+                        size="small"
+                        onChange={(e) => setPhoneTypeSelected(e.target.value)}
+                        value={phoneTypeSelected}
+                        className="w-full"
+                        displayEmpty
+                      >
+                        <MenuItem selected value="0">
+                          Select Type
+                        </MenuItem>
+                        <MenuItem value="mobile">Mobile</MenuItem>
+                        <MenuItem value="landline">Landline</MenuItem>
+                      </TextField>
+                    </div>
+                    <div className="flex items-center">
+
+                      <div class="flex items-center">
+                        <div className="h-[12px] bg-[#da1f26a3] w-[12px] mr-2">
+                          </div>
+                        <p className="filter-label">Manager</p>
+                      </div>
+                      <TextField
+                        id="Manager"
+                        select
+                        value={Manager}
+                        onChange={(e) => setManager(e.target.value)}
+                        size="small"
+                        fullWidth
+                        displayEmpty
+                      >
+                        <MenuItem value="0">Select Manager</MenuItem>
+
+                        {Managers?.map((person, index) => (
+                          <MenuItem key={index} value={person?.id}>
+                            {person?.userName}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                    </div>
+                    <div className="flex items-center">
+                      
+                      <div class="flex items-center">
+                        <div className="h-[12px] bg-[#da1f26a3] w-[12px] mr-2">
+                          </div>
+                        <p className="filter-label">Salesperson</p>
+                      </div>
+                      <TextField
+                        select
+                        id="SalesPerson"
+                        value={SalesPerson2}
+                        onChange={(e) => setSalesPerson2(e.target.value)}
+                        size="small"
+                        className="w-full"
+                        fullWidth
+                        displayEmpty
+                      >
+                        <MenuItem value="0">Select Agent</MenuItem>
+                        {SalesPerson[`manager-${Manager}`]?.map(
+                          (agent, index) => (
+                            <MenuItem key={index} value={agent?.id}>
+                              {agent?.userName}
+                            </MenuItem>
+                          )
+                        )}
+                      </TextField>
+                    </div>
+                  </div>
+                  <div className="grid w-full flex-1 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 pb-3">
+                    {contacts?.map((item, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className={`${
+                            currentMode === "dark"
+                              ? "bg-gray-900 text-white"
+                              : "bg-gray-200 text-black"
+                          } p-3 rounded-md `}
+                        >
+                          {item?.profile_picture ? (
+                            <img
+                              src={`${item?.profile_picture}`}
+                              className="rounded-md cursor-pointer h-[50px] w-[50px] object-cover"
+                              alt=""
+                            />
+                          ) : (
+                            <img
+                              src="/favicon.png"
+                              className="rounded-md cursor-pointer h-[50px] w-[50px] object-cover"
+                              alt=""
+                            />
+                          )}
+                          <div className="mt-2 space-y-1 overflow-hidden">
+                            <h1 className="font-bold capitalize">
+                              {item.userName}
+                            </h1>
+                            <p className="text-sm capitalize">
+                              {item.position}
+                            </p>
+                            {/* <p className="text-sm font-semibold text-red-600">
+                              {item.userName}
+                            </p> */}
+                            <p className="text-sm">{item.userPhone}</p>
+                            <p className="text-sm">{item.userEmail}</p>
+                            <p className="text-sm">{item.userContact}</p>
+                            {item?.status === 0 ? (
+                              <p className="text-sm text-red-600">Deactive</p>
+                            ) : (
+                              <p className="text-sm text-green-600">Active</p>
+                            )}
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </Box>
               </div>
             </div>
 
