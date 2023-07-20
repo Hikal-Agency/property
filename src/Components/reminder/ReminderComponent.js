dc0751dd66f43384b73f6d49e63b66ce314b7c6b
ubaidawan <malikunknown633@gmail.com>
2023-07-20 15:06:54 +0500

    fixed loading issues in reminder

--- src/Components/reminder/ReminderComponent.js
+++ src/Components/reminder/ReminderComponent.js
@@ -11,22 +11,117 @@ import axios from "../../axoisConfig";
 import { ToastContainer, toast } from "react-toastify";
 import { BsBuilding } from "react-icons/bs";
 import SingleLead from "../Leads/SingleLead";
+import moment from "moment";
+import { useState } from "react";
 
-const ReminderComponent = ({ reminder, handleButtonClick, handleClick }) => {
+const ReminderComponent = ({
+  reminder,
+  handleClick,
+  isLoading,
+  fetchRminders,
+}) => {
+  console.log("reminders component: ", reminder);
   const { currentMode, BACKEND_URL, User } = useStateContext();
+  const [isModalOpened, setIsModalOpened] = useState(false);
+  const [completeLoading, setCompletebtnLoading] = useState(false);
+  const [cancleLoading, setCanclebtnLoading] = useState(false);
+  const [openleadModel, setOpenLeadModel] = useState(false);
+  const [leadData, setLeadData] = useState(null);
+  // const [reminder, setReminder] = useState([]);
+  const token = localStorage.getItem("auth-token");
+  const handleLeadModelOpen = () => setOpenLeadModel(true);
+  const handleLeadModelClose = () => setOpenLeadModel(false);
+  const [loadingStates, setLoadingStates] = useState({});
+
+  const UpdateReminder = async (value, id) => {
+    console.log("value :", value);
+    let reminderStatus;
+
+    if (value === 1) {
+      setCompletebtnLoading(true);
+      reminderStatus = "Completed";
+    } else {
+      setCanclebtnLoading(true);
+      reminderStatus = "Cancelled";
+    }
+
+    const ReminderUpdate = new FormData();
+    ReminderUpdate.append("reminder_status", reminderStatus);
+
+    try {
+      const reminders = await axios.post(
+        `${BACKEND_URL}/reminders/${id}`,
+        ReminderUpdate,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+
+      setCanclebtnLoading(false);
+      setCompletebtnLoading(false);
+
+      toast.success("Successfully update reminder.", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+
+      fetchRminders(token);
+    } catch (error) {
+      setCanclebtnLoading(false);
+      setCompletebtnLoading(false);
+      console.log("Reminder error: ", error);
+      toast.error("Unable to Update Reminders.", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
+
+  const handleButtonClick = async (event, status, meetingId) => {
+    event.stopPropagation();
+    console.log("i am clicked: ", event, status, meetingId);
+    // Set loading state to true for the current meeting button that is clicked
+    setLoadingStates((prevLoadingStates) => ({
+      ...prevLoadingStates,
+      [meetingId]: true,
+    }));
+
+    await UpdateReminder(status, meetingId);
+
+    // After the API call is complete, set loading state to false for the current meeting button
+    setLoadingStates((prevLoadingStates) => ({
+      ...prevLoadingStates,
+      [meetingId]: false,
+    }));
+  };
 
   return (
-    <div>
+    <>
       <div
-        key={meeting.id}
+        key={reminder.id}
         className={`w-[350px] flex flex-col justify-between ${
           currentMode === "dark" ? "bg-black" : "bg-white"
         } rounded-md mb-3 cursor-pointer`}
-        onClick={() => handleClick(meeting?.lead_id)}
+        onClick={() => handleClick(reminder?.lead_id)}
       >
         <div className="px-5 py-5 space-y-3">
           <h2 className="text-main-red-color text-md font-bold">
-            {meeting?.leadName || "No Lead Name"}
+            {reminder?.leadName || "No Lead Name"}
           </h2>
           {/* <div className="flex items-center space-x-1">
                   <BsBuilding
@@ -35,8 +130,8 @@ const ReminderComponent = ({ reminder, handleButtonClick, handleClick }) => {
                     }`}
                   />
                   <p className="text-sm mr-3">
-                    {meeting?.project} {meeting?.enquiryType}{" "}
-                    {meeting?.leadType} {meeting?.leadFor}
+                    {reminder?.project} {reminder?.enquiryType}{" "}
+                    {reminder?.leadType} {reminder?.leadFor}
                   </p>
                 </div> */}
           <div className="w-full flex justify-between items-center">
@@ -46,10 +141,10 @@ const ReminderComponent = ({ reminder, handleButtonClick, handleClick }) => {
                   currentMode === "dark" ? "text-white" : "text-black"
                 }`}
               />
-              {meeting?.reminder_date && meeting?.reminder_time ? (
+              {reminder?.reminder_date && reminder?.reminder_time ? (
                 <p className="text-sm mr-3">{`${
-                  meeting?.reminder_time
-                }, ${moment(meeting?.reminder_date).format("MMMM D, Y")}`}</p>
+                  reminder?.reminder_time
+                }, ${moment(reminder?.reminder_date).format("MMMM D, Y")}`}</p>
               ) : (
                 "No time and date."
               )}
@@ -63,7 +158,7 @@ const ReminderComponent = ({ reminder, handleButtonClick, handleClick }) => {
                 }`}
               />
               <p className="text-sm mr-3">
-                {meeting?.reminder_note || "No Notes"}
+                {reminder?.reminder_note || "No Notes"}
               </p>
             </div>
           </div>
@@ -73,10 +168,10 @@ const ReminderComponent = ({ reminder, handleButtonClick, handleClick }) => {
             <IconButton
               style={{ backgroundColor: "#4CAF50", color: "white" }}
               className="rounded-full"
-              onClick={(event) => handleButtonClick(event, 0, meeting?.id)}
-              disabled={isLoading}
+              onClick={(event) => handleButtonClick(event, 1, reminder?.id)}
+              disabled={completeLoading}
             >
-              {isLoading ? (
+              {completeLoading ? (
                 <CircularProgress color="inherit" size={20} />
               ) : (
                 <CheckIcon />
@@ -88,10 +183,10 @@ const ReminderComponent = ({ reminder, handleButtonClick, handleClick }) => {
             <IconButton
               style={{ backgroundColor: "#DC2626", color: "white" }}
               className="rounded-full"
-              onClick={(event) => handleButtonClick(event, 0, meeting?.id)}
-              disabled={isLoading}
+              onClick={(event) => handleButtonClick(event, 0, reminder?.id)}
+              disabled={cancleLoading}
             >
-              {isLoading ? (
+              {cancleLoading ? (
                 <CircularProgress color="inherit" size={20} />
               ) : (
                 <CloseIcon />
@@ -100,7 +195,7 @@ const ReminderComponent = ({ reminder, handleButtonClick, handleClick }) => {
           </Tooltip>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
