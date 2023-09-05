d9bc6ee56af686fd2a2ef32b1c46632b3488e3e3
jimuskanin <workofmuskan@gmail.com>
2023-09-05 15:22:22 +0400

    OTP ICONS

--- src/Components/Leads/NewlLeads.js
+++ src/Components/Leads/NewlLeads.js
@@ -27,7 +27,14 @@ import { FaSnapchat } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { BiImport } from "react-icons/bi";
 import { FcGoogle } from "react-icons/fc";
-import { BsPersonCircle, BsSnow2, BsTrash } from "react-icons/bs";
+import { 
+  BsPersonCircle, 
+  BsSnow2, 
+  BsTrash,
+  BsShieldX,
+  BsShieldCheck,
+  BsShieldMinus 
+} from "react-icons/bs";
 import { IoIosAlert } from "react-icons/io";
 import {
   RxCheckCircled,
@@ -308,7 +315,7 @@ const Newleads = ({
                   className={`mx-1 w-full h-full flex justify-center items-center text-center`}
                 >
                   <span className="text-[#238e41] p-1 text-center">
-                    <RxCheckCircled size={16} />
+                    <BsShieldCheck size={16} />
                   </span>
                 </div>
               </Tooltip>
@@ -320,7 +327,7 @@ const Newleads = ({
                   className={`mx-1 w-full h-full flex justify-center items-center text-center`}
                   >
                   <span className="text-[#DA1F26] p-1 text-center">
-                    <RxCrossCircled size={16} />
+                    <BsShieldX size={16} />
                   </span>
                 </div>
               </Tooltip>
@@ -333,7 +340,7 @@ const Newleads = ({
                   className={`mx-1 w-full h-full flex justify-center items-center text-center`}
                 >
                   <span className="text-[#AAAAAA] p-1 text-center">
-                    <RxQuestionMarkCircled size={16} />
+                    <BsShieldMinus size={16} />
                   </span>
                 </div>
               </Tooltip>
