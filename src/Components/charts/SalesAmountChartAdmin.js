6b4e7cd5b18de056920d63f0304e73548ecfe96b
ubaidawan <malikunknown633@gmail.com>
2023-09-12 14:09:57 +0500

    old chart

--- src/Components/charts/SalesAmountChartAdmin.js
+++ src/Components/charts/SalesAmountChartAdmin.js
@@ -35,163 +35,163 @@ const SalesAmountChartAdmin = ({ selectedMonthSales }) => {
     });
   }, [Sales_chart_data]);
 
-  // return (
-  //   <span>
-  //     {currentMode === "dark" ? (
-  //       <Bar
-  //         data={salesData}
-  //         options={{
-  //           indexAxis: "y",
-  //           elements: {
-  //             bar: {
-  //               borderWidth: 0,
-  //             },
-  //           },
-  //           responsive: true,
-  //           plugins: {
-  //             legend: {
-  //               display: false,
-  //             },
-  //             title: {
-  //               display: false,
-  //             },
-  //           },
-  //           color: "#AAAAAA",
-  //           scales: {
-  //             x: {
-  //               beginAtZero: true,
-  //               ticks: {
-  //                 stepSize: 1000000,
-  //                 color: "#AAAAAA",
-  //               },
-  //               grid: {
-  //                 display: true,
-  //                 color: "#424242",
-  //               },
-  //             },
-  //             y: {
-  //               beginAtZero: true,
-  //               ticks: {
-  //                 stepSize: 1,
-  //                 color: "#AAAAAA",
-  //               },
-  //               grid: {
-  //                 display: false,
-  //               },
-  //             },
-  //           },
-  //         }}
-  //       />
-  //     ) : (
-  //       <Bar
-  //         data={salesData}
-  //         options={{
-  //           indexAxis: "y",
-  //           elements: {
-  //             bar: {
-  //               borderWidth: 0,
-  //             },
-  //           },
-  //           responsive: true,
-  //           plugins: {
-  //             legend: {
-  //               display: false,
-  //             },
-  //             title: {
-  //               display: false,
-  //             },
-  //           },
-  //           color: "#333333",
-  //           scales: {
-  //             x: {
-  //               beginAtZero: true,
-  //               ticks: {
-  //                 stepSize: 1000000,
-  //                 color: "#333333",
-  //               },
-  //               grid: {
-  //                 display: true,
-  //               },
-  //             },
-  //             y: {
-  //               beginAtZero: true,
-  //               ticks: {
-  //                 stepSize: 1,
-  //                 color: "#333333",
-  //               },
-  //               grid: {
-  //                 display: false,
-  //               },
-  //             },
-  //           },
-  //         }}
-  //       />
-  //     )}
-  //   </span>
-  // );
-
-  const chartData = [
-    {
-      data: Sales_chart_data?.map((data, index) => ({
-        x: data?.userName, // Use the sales data as the x-coordinate
-        y: Math.random() * 100, // Generate a random y-coordinate for demonstration
-        z: Math.random() * 100, // Generate a random z-coordinate for demonstration
-      })),
-    },
-  ];
-
-  const chartOptions = {
-    chart: {
-      height: 400,
-      type: "bubble",
-      background: "transparent",
-    },
-
-    fill: {
-      type: "gradient",
-    },
-    xaxis: {
-      // title: {
-      //   text: "Sales in AED",
-      // },
-      labels: {
-        style: {
-          colors: currentMode === "dark" ? "#ffffff" : "#000000",
-        },
-        rotateAlways: true, // Tilt the X-axis labels
-        className: "x-axis-labels",
-      },
-    },
-    yaxis: {
-      // title: {
-      //   text: "Y-Axis",
-      // },
-      labels: {
-        style: {
-          colors: currentMode === "dark" ? "#ffffff" : "#000000",
-        },
-      },
-    },
-    markers: {
-      size: 6,
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    title: {
-      // text: "3D Bubble Chart",
-    },
-    theme: {
-      // mode: currentMode,
-      palette: "palette2",
-    },
-  };
-
   return (
-    <div>
-      <ReactApexChart options={chartOptions} series={chartData} type="bubble" />
-    </div>
+    <span>
+      {currentMode === "dark" ? (
+        <Bar
+          data={salesData}
+          options={{
+            indexAxis: "y",
+            elements: {
+              bar: {
+                borderWidth: 0,
+              },
+            },
+            responsive: true,
+            plugins: {
+              legend: {
+                display: false,
+              },
+              title: {
+                display: false,
+              },
+            },
+            color: "#AAAAAA",
+            scales: {
+              x: {
+                beginAtZero: true,
+                ticks: {
+                  stepSize: 1000000,
+                  color: "#AAAAAA",
+                },
+                grid: {
+                  display: true,
+                  color: "#424242",
+                },
+              },
+              y: {
+                beginAtZero: true,
+                ticks: {
+                  stepSize: 1,
+                  color: "#AAAAAA",
+                },
+                grid: {
+                  display: false,
+                },
+              },
+            },
+          }}
+        />
+      ) : (
+        <Bar
+          data={salesData}
+          options={{
+            indexAxis: "y",
+            elements: {
+              bar: {
+                borderWidth: 0,
+              },
+            },
+            responsive: true,
+            plugins: {
+              legend: {
+                display: false,
+              },
+              title: {
+                display: false,
+              },
+            },
+            color: "#333333",
+            scales: {
+              x: {
+                beginAtZero: true,
+                ticks: {
+                  stepSize: 1000000,
+                  color: "#333333",
+                },
+                grid: {
+                  display: true,
+                },
+              },
+              y: {
+                beginAtZero: true,
+                ticks: {
+                  stepSize: 1,
+                  color: "#333333",
+                },
+                grid: {
+                  display: false,
+                },
+              },
+            },
+          }}
+        />
+      )}
+    </span>
   );
+
+  // const chartData = [
+  //   {
+  //     data: Sales_chart_data?.map((data, index) => ({
+  //       x: data?.userName, // Use the sales data as the x-coordinate
+  //       y: Math.random() * 100, // Generate a random y-coordinate for demonstration
+  //       z: Math.random() * 100, // Generate a random z-coordinate for demonstration
+  //     })),
+  //   },
+  // ];
+
+  // const chartOptions = {
+  //   chart: {
+  //     height: 400,
+  //     type: "bubble",
+  //     background: "transparent",
+  //   },
+
+  //   fill: {
+  //     type: "gradient",
+  //   },
+  //   xaxis: {
+  //     // title: {
+  //     //   text: "Sales in AED",
+  //     // },
+  //     labels: {
+  //       style: {
+  //         colors: currentMode === "dark" ? "#ffffff" : "#000000",
+  //       },
+  //       rotateAlways: true, // Tilt the X-axis labels
+  //       className: "x-axis-labels",
+  //     },
+  //   },
+  //   yaxis: {
+  //     // title: {
+  //     //   text: "Y-Axis",
+  //     // },
+  //     labels: {
+  //       style: {
+  //         colors: currentMode === "dark" ? "#ffffff" : "#000000",
+  //       },
+  //     },
+  //   },
+  //   markers: {
+  //     size: 6,
+  //   },
+  //   dataLabels: {
+  //     enabled: false,
+  //   },
+  //   title: {
+  //     // text: "3D Bubble Chart",
+  //   },
+  //   theme: {
+  //     // mode: currentMode,
+  //     palette: "palette2",
+  //   },
+  // };
+
+  // return (
+  //   <div>
+  //     <ReactApexChart options={chartOptions} series={chartData} type="bubble" />
+  //   </div>
+  // );
 };
 
 export default SalesAmountChartAdmin;
