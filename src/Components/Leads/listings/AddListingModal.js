1e674af83b6966ee6dc17c808e93dfbe83605846
ubaidawan <malikunknown633@gmail.com>
2023-09-21 16:34:39 +0500

    listings forms

--- src/Components/Leads/listings/AddListingModal.js
+++ src/Components/Leads/listings/AddListingModal.js
@@ -1,19 +1,33 @@
-import { Modal, Backdrop, IconButton } from "@mui/material";
+import {
+  Modal,
+  Backdrop,
+  IconButton,
+  CircularProgress,
+  Box,
+  Button,
+  MenuItem,
+  TextField,
+} from "@mui/material";
 import { IoMdClose } from "react-icons/io";
 import { useStateContext } from "../../../context/ContextProvider";
+import { useState } from "react";
+import LocationPicker from "../../meetings/LocationPicker";
+import ListingLocation from "./ListingLocation";
+import { MdFileUpload } from "react-icons/md";
 
 const style = {
   transform: "translate(-50%, -50%)",
   boxShadow: 24,
 };
 
-const AddListingModal = ({
-  setListingModalOpen,
-  handleCloseListingModal,
-  FetchLeads,
-}) => {
-  const { currentMode } = useStateContext();
-
+const AddListingModal = ({ setListingModalOpen, handleCloseListingModal }) => {
+  const { currentMode, darkModeColors, User, BACKEND_URL } = useStateContext();
+  const [loading, setloading] = useState(false);
+  const [listingLocation, setLisitingLocation] = useState({
+    lat: 0,
+    lng: 0,
+    addressText: "",
+  });
   return (
     <>
       <Modal
@@ -30,7 +44,7 @@ const AddListingModal = ({
       >
         <div
           style={style}
-          className={`w-[calc(100%-20px)] h-[80%] overflow-y-scroll md:w-[80%] border-2 border-solid shadow-lg  ${
+          className={`w-[calc(100%-20px)] h-[80%] overflow-y-scroll md:w-[50%] border-2 border-solid shadow-lg  ${
             currentMode === "dark"
               ? "bg-black border-gray-800"
               : "bg-white border-gray-200"
@@ -47,6 +61,346 @@ const AddListingModal = ({
           >
             <IoMdClose size={18} />
           </IconButton>
+
+          <div className="w-full flex items-center py-1 mb-2">
+            {/* <div className="bg-[#DA1F26] h-10 w-1 rounded-full mr-2 my-1"></div> */}
+            <h1
+              className={`text-lg font-semibold bg-[#DA1F26] py-2 px-6 ${
+                currentMode === "dark" ? "text-white" : "text-black"
+              }`}
+            >
+              Secondary
+            </h1>
+            <h1
+              className={`text-lg font-semibold ml-3 ${
+                currentMode === "dark" ? "text-white" : "text-black"
+              }`}
+            >
+              Add Property
+            </h1>
+          </div>
+
+          <div className="mx-auto ">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                // AddLead();
+              }}
+              disabled={loading ? true : false}
+            >
+              <div className="grid grid-cols-1 mt-9 sm:grid-cols-1 md:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 gap-5 px-4 md:px-10 ">
+                <div className="px-4">
+                  <Box sx={darkModeColors}>
+                    <h4
+                      className={`${
+                        currentMode === "dark" ? "text-red-600" : "text-black"
+                      } text-center font-semibold pb-5`}
+                    >
+                      Seller Details
+                    </h4>
+
+                    <TextField
+                      id="notes"
+                      type={"text"}
+                      label="Legal Name"
+                      className="w-full"
+                      sx={{
+                        "&": {
+                          marginBottom: "1.25rem !important",
+                        },
+                      }}
+                      variant="outlined"
+                      size="small"
+                      required
+                      // value={LeadNotes}
+                      // onChange={(e) => setLeadNotes(e.target.value)}
+                    />
+                    <TextField
+                      id="notes"
+                      type={"text"}
+                      label="Contacts"
+                      className="w-full"
+                      sx={{
+                        "&": {
+                          marginBottom: "1.25rem !important",
+                        },
+                      }}
+                      variant="outlined"
+                      size="small"
+                      required
+                      // value={LeadNotes}
+                      // onChange={(e) => setLeadNotes(e.target.value)}
+                    />
+                    <TextField
+                      id="notes"
+                      type={"text"}
+                      label="Email"
+                      className="w-full"
+                      sx={{
+                        "&": {
+                          marginBottom: "1.25rem !important",
+                        },
+                      }}
+                      variant="outlined"
+                      size="small"
+                      required
+                      // value={LeadNotes}
+                      // onChange={(e) => setLeadNotes(e.target.value)}
+                    />
+                    <TextField
+                      id="notes"
+                      type={"text"}
+                      label="Property Price"
+                      className="w-full"
+                      sx={{
+                        "&": {
+                          marginBottom: "1.25rem !important",
+                        },
+                      }}
+                      variant="outlined"
+                      size="small"
+                      required
+                      // value={LeadNotes}
+                      // onChange={(e) => setLeadNotes(e.target.value)}
+                    />
+                  </Box>
+                </div>
+
+                <div className="px-4">
+                  <Box sx={darkModeColors}>
+                    <h4
+                      className={`${
+                        currentMode === "dark" ? "text-red-600" : "text-black"
+                      } text-center font-semibold pb-5`}
+                    >
+                      Project details
+                    </h4>
+
+                    <TextField
+                      id="property-type"
+                      // value={PropertyType}
+                      label="Property type"
+                      // onChange={ChangePropertyType}
+                      size="small"
+                      className="w-full mb-5"
+                      displayEmpty
+                      sx={{
+                        "&": {
+                          marginBottom: "1.25rem !important",
+                        },
+                      }}
+                      select
+                    >
+                      <MenuItem value="" disabled>
+                        Property type
+                        <span className="ml-1" style={{ color: "red" }}>
+                          *
+                        </span>
+                      </MenuItem>
+                      <MenuItem value={"Apartment"}>Apartment</MenuItem>
+                      <MenuItem value={"Villa"}>Villa</MenuItem>
+                      <MenuItem value={"penthouse"}>Penthouse</MenuItem>
+                      <MenuItem value={"mansion"}>Mansion</MenuItem>
+                      <MenuItem value={"Commercial"}>Commercial</MenuItem>
+                      <MenuItem value={"Townhouse"}>TownHouse</MenuItem>
+                    </TextField>
+
+                    <TextField
+                      id="notes"
+                      type={"text"}
+                      label="Project/Name of Building"
+                      className="w-full"
+                      sx={{
+                        "&": {
+                          marginBottom: "1.25rem !important",
+                        },
+                      }}
+                      variant="outlined"
+                      size="small"
+                      required
+                      // value={LeadNotes}
+                      // onChange={(e) => setLeadNotes(e.target.value)}
+                    />
+
+                    <TextField
+                      id="enquiry"
+                      label="Number Of Bedrooms"
+                      // value={EnquiryType}
+                      // onChange={ChangeEnquiryType}
+                      size="small"
+                      className="w-full"
+                      sx={{
+                        "&": {
+                          marginBottom: "1.25rem !important",
+                        },
+                      }}
+                      displayEmpty
+                      select
+                    >
+                      <MenuItem value="" disabled>
+                        Enquiry about
+                        <span className="ml-1" style={{ color: "red" }}>
+                          *
+                        </span>
+                      </MenuItem>
+                      <MenuItem value={"Studio"}>Studio</MenuItem>
+                      <MenuItem value={"1 Bedroom"}>1 Bedroom</MenuItem>
+                      <MenuItem value={"2 Bedrooms"}>2 Bedrooms</MenuItem>
+                      <MenuItem value={"3 Bedrooms"}>3 Bedrooms</MenuItem>
+                      <MenuItem value={"4 Bedrooms"}>4 Bedrooms</MenuItem>
+                      <MenuItem value={"5 Bedrooms"}>5 Bedrooms</MenuItem>
+                      <MenuItem value={"6 Bedrooms"}>6 Bedrooms</MenuItem>
+                    </TextField>
+
+                    <TextField
+                      id="for"
+                      // value={ForType}
+                      label="Purpose of enquiry"
+                      // onChange={ChangeForType}
+                      size="small"
+                      className="w-full"
+                      sx={{
+                        "&": {
+                          marginBottom: "1.25rem !important",
+                        },
+                      }}
+                      displayEmpty
+                      select
+                    >
+                      <MenuItem value="" disabled>
+                        Purpose of enquiry
+                        <span className="ml-1" style={{ color: "red" }}>
+                          *
+                        </span>
+                      </MenuItem>
+                      <MenuItem value={"Investment"}>Investment</MenuItem>
+                      <MenuItem value={"End-user"}>End-User</MenuItem>
+                    </TextField>
+                  </Box>
+                </div>
+
+                <div className="px-4">
+                  <Box sx={darkModeColors}>
+                    <h4
+                      className={`${
+                        currentMode === "dark" ? "text-red-600" : "text-black"
+                      } text-center font-semibold pb-5`}
+                    >
+                      Other Details
+                    </h4>
+
+                    <TextField
+                      id="LeadEmailAddress"
+                      type={"text"}
+                      label="Address"
+                      className="w-full"
+                      sx={{
+                        "&": {
+                          marginBottom: "1.25rem !important",
+                        },
+                      }}
+                      variant="outlined"
+                      size="small"
+                      // error={emailError && emailError}
+                      // helperText={emailError && emailError}
+                      // value={LeadEmail}
+                      // onChange={handleEmail}
+                    />
+                    <TextField
+                      id="LeadEmailAddress"
+                      type={"text"}
+                      label="Area"
+                      className="w-full"
+                      sx={{
+                        "&": {
+                          marginBottom: "1.25rem !important",
+                        },
+                      }}
+                      variant="outlined"
+                      size="small"
+                      // error={emailError && emailError}
+                      // helperText={emailError && emailError}
+                      // value={LeadEmail}
+                      // onChange={handleEmail}
+                    />
+
+                    <ListingLocation
+                      listingLocation={listingLocation}
+                      currLocByDefault={true}
+                      setLisitingLocation={setLisitingLocation}
+                    />
+                  </Box>
+                </div>
+              </div>
+
+              <div className="w-full flex justify-center mr-4 items-center my-4 space-x-5">
+                <label htmlFor="contained-button-file">
+                  <Button
+                    variant="contained"
+                    size="medium"
+                    className="bg-main-red-color w-full  text-white rounded-lg py-3 font-semibold my-3"
+                    style={{
+                      backgroundColor: "#111827",
+                      color: "#ffffff",
+                      border: "1px solid #DA1F26",
+                    }}
+                    component="span"
+                    disabled={loading ? true : false}
+                    startIcon={loading ? null : <MdFileUpload />}
+                  >
+                    <span>Upload Image</span>
+                  </Button>
+                </label>
+                <label htmlFor="contained-button-file">
+                  <Button
+                    variant="contained"
+                    size="medium"
+                    className="bg-main-red-color w-full text-white rounded-lg py-3 font-semibold my-3"
+                    style={{
+                      backgroundColor: "#111827",
+                      color: "#ffffff",
+                      border: "1px solid #DA1F26",
+                    }}
+                    component="span"
+                    disabled={loading ? true : false}
+                    startIcon={loading ? null : <MdFileUpload />}
+                  >
+                    <span>Upload Document</span>
+                  </Button>
+                </label>
+              </div>
+
+              <div
+                className={`${
+                  currentMode === "dark" ? "bg-black" : "bg-white"
+                } px-5 mx-5 py-2 text-center sm:px-6`}
+              >
+                <Button
+                  ripple={true}
+                  size="lg"
+                  type="submit"
+                  disabled={loading ? true : false}
+                  style={{
+                    background: "#da1f26",
+                    color: "#ffffff",
+                    marginTop: "10px",
+                    width: "100%",
+                    borderRadius: "6px",
+                  }}
+                >
+                  {loading ? (
+                    <CircularProgress
+                      size={20}
+                      sx={{ color: "white" }}
+                      className="text-white"
+                    />
+                  ) : (
+                    <span>Add property for secondary market</span>
+                  )}
+                </Button>
+              </div>
+            </form>
+          </div>
         </div>
       </Modal>
     </>
---
+++ src/Components/Leads/listings/ListingAutoComplete.js
@@ -0,0 +1,89 @@
+import { useEffect } from "react";
+import { TextField } from "@mui/material";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import useOnclickOutside from "react-cool-onclickoutside";
+import { useStateContext } from "../../../context/ContextProvider";
+
+const ListingAutoComplete = ({
+  isDisabled,
+  defaultLocation,
+  setMeetingLocation,
+}) => {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    debounce: 300,
+  });
+  const { currentMode } = useStateContext();
+  const ref = useOnclickOutside(() => {
+    clearSuggestions();
+  });
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSelect =
+    ({ description }) =>
+    () => {
+      setValue(description, false);
+      clearSuggestions();
+
+      getGeocode({ address: description }).then((results) => {
+        const { lat, lng } = getLatLng(results[0]);
+        setMeetingLocation((meetingLocation) => {
+          return { ...meetingLocation, lat, lng };
+        });
+      });
+    };
+
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      console.log(suggestion);
+      const {
+        place_id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
+
+      return (
+        <li key={place_id} onClick={handleSelect(suggestion)}>
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      );
+    });
+
+  useEffect(() => {
+    setValue(defaultLocation);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [defaultLocation]);
+
+  return (
+    <div ref={ref}>
+      <TextField
+        type={"text"}
+        fullWidth
+        sx={{
+          "& input": {
+            color: currentMode === "dark" ? "white" : "black",
+            fontFamily: "Noto Kufi Arabic",
+          },
+        }}
+        disabled={isDisabled ? true : !ready}
+        onChange={handleInput}
+        required
+        placeholder="Search Location"
+        value={value}
+      />
+      {status === "OK" && !isDisabled && <ul>{renderSuggestions()}</ul>}
+    </div>
+  );
+};
+
+export default ListingAutoComplete;
