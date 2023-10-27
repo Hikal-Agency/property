79d9aaa447568ceded6e194dc23aef426a368d5d
ubaidawan <malikunknown633@gmail.com>
2023-10-27 11:31:06 +0500

    adding search in timezone

--- src/Components/Navbar/Clock.js
+++ src/Components/Navbar/Clock.js
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment-timezone";
-import { MenuItem, Select, Box } from "@mui/material";
+import {
+  MenuItem,
+  Select,
+  Box,
+  TextField,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
 import { useStateContext } from "../../context/ContextProvider";
 
 import AnalogClock from "./AnalogClock";
+import { BsSearch } from "react-icons/bs";
 
-const Clock = ({handleClose}) => {
+const Clock = ({ handleClose }) => {
   const [currentTime, setCurrentTime] = useState(
-    localStorage.getItem("timezone") ? moment().tz(localStorage.getItem("timezone")).format("D/MM/YYYY, h:mm:ss a [GMT]Z") :
-    moment().tz(moment.tz.guess()).format("D/MM/YYYY, h:mm:ss a [GMT]Z")
+    localStorage.getItem("timezone")
+      ? moment()
+          .tz(localStorage.getItem("timezone"))
+          .format("D/MM/YYYY, h:mm:ss a [GMT]Z")
+      : moment().tz(moment.tz.guess()).format("D/MM/YYYY, h:mm:ss a [GMT]Z")
   );
   const [timezones, setTimezones] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
   const [selectedTimezone, setSelectedTimezone] = useState(moment.tz.guess());
-  const { currentMode, darkModeColors } = useStateContext();
+  const { currentMode, darkModeColors, t } = useStateContext();
 
   console.log("mode: ", currentMode);
 
   useEffect(() => {
-    if(localStorage.getItem("timezone")) {
+    if (localStorage.getItem("timezone")) {
       setSelectedTimezone(localStorage.getItem("timezone"));
     }
   }, []);
@@ -39,16 +52,21 @@ const Clock = ({handleClose}) => {
     };
   }, [selectedTimezone]);
 
+  const filteredTimezones = timezones?.filter((timezone) =>
+    timezone.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  console.log("filtered timezone list: ", filteredTimezones);
+
   const handleTimezoneChange = (e) => {
+    console.log("timzone selected : ", e.target.value);
     setSelectedTimezone(e.target.value);
     localStorage.setItem("timezone", e.target.value);
   };
 
-  
-
   return (
     <div
-    onMouseLeave={handleClose}
+      onMouseLeave={handleClose}
       style={{
         margin: 0,
         padding: "0.5rem 0.5rem",
@@ -58,17 +76,20 @@ const Clock = ({handleClose}) => {
               ? "#1C1C1C !important"
               : "#1C1C1C !important",
         },
-        "& .MuiPaper-root-MuiPopover-paper-MuiMenu-paper, .MuiList-root .clock-div": {
-          backgroundColor:
-            currentMode === "dark"
-              ? "#1C1C1C !important"
-              : "#1C1C1C !important",
-        }
+        "& .MuiPaper-root-MuiPopover-paper-MuiMenu-paper, .MuiList-root .clock-div":
+          {
+            backgroundColor:
+              currentMode === "dark"
+                ? "#1C1C1C !important"
+                : "#1C1C1C !important",
+          },
       }}
-      className={`${currentMode === "dark" ? "bg-[#1C1C1C]" : "bg-[#000000]"} clock-div`}
+      className={`${
+        currentMode === "dark" ? "bg-[#1C1C1C]" : "bg-[#000000]"
+      } clock-div`}
     >
       <div className="flex justify-end mb-2">
-        <Box sx={darkModeColors} style={{ minWidth: "30px"}} >
+        <Box sx={darkModeColors} style={{ minWidth: "30px" }}>
           <Select
             className="time-zone-select"
             sx={{
@@ -76,17 +97,67 @@ const Clock = ({handleClose}) => {
               "& .MuiSelect-select": {
                 padding: "5px 20px 5px 5px !important",
               },
-              color: currentMode === "dark" ? "#FFF !important" : "#000 !important",
+              color:
+                currentMode === "dark" ? "#FFF !important" : "#000 !important",
             }}
             size="small"
             variant="standard"
             value={selectedTimezone}
-            onChange={handleTimezoneChange}
+            onClick={handleTimezoneChange}
           >
-            {timezones?.map((timezone) => (
-              <MenuItem onClick={(e) => e.stopPropagation()} key={timezone} value={timezone}>
-                {timezone}
-              </MenuItem>
+            <MenuItem
+              onKeyDown={(e) => {
+                e.stopPropagation();
+                // e.preventDefault();
+              }}
+            >
+              {/* <Box sx={darkModeColors}> */}
+              <TextField
+                placeholder={t("search_timezone")}
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                sx={{
+                  "& input": {
+                    border: "0",
+                  },
+                }}
+                variant="standard"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <IconButton
+                        sx={{ padding: 1 }}
+                        // onClick={(e) => {
+                        //   e.preventDefault();
+                        //   const inputValue =
+                        //     searchRef.current.querySelector("input").value;
+                        //   if (inputValue) {
+                        //     fetchUsers(inputValue);
+                        //   }
+                        // }}
+                      >
+                        <BsSearch className={`text-[#AAAAAA]`} size={18} />
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                onClick={(event) => {
+                  event.stopPropagation();
+                }}
+              />
+              {/* </Box> */}
+            </MenuItem>
+            {filteredTimezones?.map((timezone) => (
+              <>
+                <MenuItem
+                  selected={timezone === selectedTimezone}
+                  onKeyDown={(e) => e.stopPropagation()}
+                  key={timezone}
+                  value={timezone}
+                >
+                  {timezone}
+                </MenuItem>
+              </>
             ))}
           </Select>
         </Box>
