11ace907460be4f0f566724554bd68675dadfc21
ubaidawan <malikunknown633@gmail.com>
2023-11-07 14:03:04 +0500

    search with results in navbar

---
+++ src/utils/axiosSearch.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const makeRequestCreator = () => {
+  let token;
+
+  return async (query, authToken) => {
+    // Allow users to pass the Bearer token
+    // Check if we made a request
+    if (token) {
+      // Cancel the previous request before making a new request
+      token.cancel("Request canceled"); // Pass a cancellation message
+    }
+    // Create a new CancelToken
+    token = axios.CancelToken.source();
+    try {
+      const config = {
+        cancelToken: token.token,
+        headers: {
+          Authorization: `Bearer ${authToken}`, // Add the Bearer token to the headers
+        },
+      };
+      const res = await axios(query, config);
+      const result = res.data;
+      return result;
+    } catch (error) {
+      if (axios.isCancel(error)) {
+        // Handle if the request was canceled
+        console.log("Request canceled", error.message);
+      } else {
+        // Handle usual errors
+        console.log("Something went wrong: ", error.message);
+      }
+    }
+  };
+};
+
+export const search = makeRequestCreator();
