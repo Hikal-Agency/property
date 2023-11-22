3a8efe7bf7219200e36f7b42a450f9d1c71dcebb
ubaidawan <malikunknown633@gmail.com>
2023-11-22 16:50:59 +0500

    add dev form

--- src/Pages/propertyPortfolio/AddDeveloper.js
+++ src/Pages/propertyPortfolio/AddDeveloper.js
@@ -12,6 +12,7 @@ import {
   Drawer,
   MenuItem,
   Box,
+  Typography,
 } from "@mui/material";
 import { useStateContext } from "../../context/ContextProvider";
 import usePermission from "../../utils/usePermission";
@@ -55,7 +56,11 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
   const [open, setOpen] = useState(false);
 
   const [loading, setLoading] = useState(false);
-  const [emailError, setEmailError] = useState(false);
+  const [devLoading, setAddDevLoading] = useState(false);
+  const [emailError, setEmailError] = useState({
+    developerEmail: false,
+    rm_email: false,
+  });
   const [error, setError] = useState(false);
   const [devData, setDevData] = useState({
     developerName: null,
@@ -67,12 +72,16 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
     rm_name: null,
     rm_contact: null,
     rm_email: null,
-    addedBy: null,
+    addedBy: User?.id,
   });
+
+  console.log("handleChange:: ", devData);
 
   const handleChange = (e) => {
     const data = e.target.value;
     const name = e.target.name;
+
+    console.log("data,name:: ", data, name);
 
     setDevData((prev) => ({
       ...prev,
@@ -82,22 +91,32 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
 
   const [isClosing, setIsClosing] = useState(false);
   const handleEmail = (e) => {
-    setEmailError(false);
+    setEmailError({
+      developerEmail: false,
+      rm_email: false,
+    });
     const value = e.target.value;
+    const key = e.target.name;
     console.log(value);
     // const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 
     const emailRegex = /^[A-Za-z0-9._+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 
     if (emailRegex.test(value)) {
-      setEmailError(false);
+      setEmailError({
+        developerEmail: false,
+        rm_email: false,
+      });
     } else {
-      setEmailError("Kindly enter a valid email.");
-      // setLeadEmail("");
-      return;
+      setEmailError({
+        [key]: "Kindly enter a valid email.",
+      });
+      // return;
     }
-    setLeadEmail(value);
-    console.log("Email state: ", LeadEmail);
+    setDevData((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
   };
 
   const handleClose = () => {
@@ -114,54 +133,90 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
   };
 
   const AddLead = () => {
-    setaddNoteloading(true);
+    setAddDevLoading(true);
     const token = localStorage.getItem("auth-token");
 
-    // const data = {
-    //   leadId: LeadData.leadId || LeadData.id,
-    //   leadNote: note || AddNoteTxt,
-    //   addedBy: User?.id,
-    //   addedByName: User?.userName,
-    // };
-    // axios
-    //   .post(`${BACKEND_URL}/leadNotes`, data, {
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       Authorization: "Bearer " + token,
-    //     },
-    //   })
-    //   .then((result) => {
-    //     console.log("Result: ");
-    //     console.log("Result: ", result);
-    //     setaddNoteloading(false);
-    //     setAddNoteTxt("");
-    //     if (!note) {
-    //       toast.success("Note added Successfully", {
-    //         position: "top-right",
-    //         autoClose: 3000,
-    //         hideProgressBar: false,
-    //         closeOnClick: true,
-    //         pauseOnHover: true,
-    //         draggable: true,
-    //         progress: undefined,
-    //         theme: "light",
-    //       });
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     setaddNoteloading(false);
-    //     console.log(err);
-    //     toast.error("Soemthing Went Wrong! Please Try Again", {
-    //       position: "top-right",
-    //       autoClose: 3000,
-    //       hideProgressBar: false,
-    //       closeOnClick: true,
-    //       pauseOnHover: true,
-    //       draggable: true,
-    //       progress: undefined,
-    //       theme: "light",
-    //     });
-    //   });
+    if (!devData?.developerName) {
+      setAddDevLoading(false);
+
+      toast.error("Developer name is required.", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      return;
+    }
+
+    if (emailError?.developerEmail || emailError?.rm_email) {
+      setAddDevLoading(false);
+
+      toast.error("Kindly enter a valid email.", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      return;
+    }
+
+    axios
+      .post(`${BACKEND_URL}/developers`, devData, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((result) => {
+        console.log("Result: ");
+        console.log("Result: ", result);
+        setAddDevLoading(false);
+
+        setDevData({
+          developerName: "",
+          founder: "",
+          ceo: "",
+          developerContact: "",
+          developerEmail: "",
+          address: "",
+          rm_name: "",
+          rm_contact: "",
+          rm_email: "",
+        });
+
+        toast.success("Developer added successfully.", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      })
+      .catch((err) => {
+        setAddDevLoading(false);
+        console.log(err);
+        toast.error("Soemthing Went Wrong! Please Try Again", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      });
   };
 
   return (
@@ -266,6 +321,7 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
                             <TextField
                               id="LeadEmailAddress"
                               name="developerName"
+                              value={devData?.developerName}
                               type={"text"}
                               label={t("form_developer_name")}
                               className="w-full"
@@ -277,12 +333,14 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
                               variant="outlined"
                               size="small"
                               required
+                              onChange={handleChange}
                             />
                             <TextField
                               id="notes"
                               type={"text"}
                               label={t("form_developer_founder")}
                               name="founder"
+                              value={devData?.founder}
                               className="w-full"
                               sx={{
                                 "&": {
@@ -291,12 +349,12 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
                               }}
                               variant="outlined"
                               size="small"
-                              //   value={LeadNotes}
-                              //   onChange={(e) => setLeadNotes(e.target.value)}
+                              onChange={handleChange}
                             />
                             <TextField
                               id="Manager"
                               type="text"
+                              value={devData?.ceo}
                               name="ceo"
                               label={t("form_developer_ceo")}
                               className="w-full"
@@ -310,6 +368,7 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
                               }}
                               variant="outlined"
                               size="small"
+                              onChange={handleChange}
                             />
                           </Box>
                         </div>
@@ -328,6 +387,7 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
                             <TextField
                               id="Project"
                               type={"text"}
+                              value={devData?.developerContact}
                               name="developerContact"
                               label={t("form_developer_contact")}
                               className="w-full"
@@ -339,10 +399,12 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
                               variant="outlined"
                               size="small"
                               //   value={LeadProject}
+                              onChange={handleChange}
                             />
                             <TextField
                               id="Project"
                               type={"text"}
+                              value={devData?.developerEmail}
                               name="developerEmail"
                               label={t("form_developer_email")}
                               className="w-full"
@@ -354,13 +416,24 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
                               variant="outlined"
                               size="small"
                               //   value={LeadProject}
+                              onChange={handleEmail}
                             />
+                            {emailError?.developerEmail && (
+                              <Typography
+                                variant="body2"
+                                color="error"
+                                className="mb-3"
+                              >
+                                {emailError?.developerEmail}
+                              </Typography>
+                            )}
 
                             <TextField
                               id="LeadEmailAddress"
-                              type={"email"}
+                              type={"text"}
+                              value={devData?.address}
                               name="address"
-                              label={t("form_developer_emailAddress")}
+                              label={t("form_developer_address")}
                               className="w-full"
                               sx={{
                                 "&": {
@@ -369,6 +442,7 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
                               }}
                               variant="outlined"
                               size="small"
+                              onChange={handleChange}
                             />
                           </Box>
                         </div>
@@ -387,6 +461,7 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
                             <TextField
                               id="LeadName"
                               type={"text"}
+                              value={devData?.rm_name}
                               name="rm_name"
                               label={t("form_developer_fullName")}
                               className="w-full"
@@ -397,34 +472,43 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
                               }}
                               variant="outlined"
                               size="small"
+                              onChange={handleChange}
                               // required
                             />
 
-                            {error && (
-                              <Typography variant="body2" color="error">
-                                {error}
-                              </Typography>
-                            )}
                             <TextField
                               id="LeadEmailAddress"
                               type={"email"}
                               label={t("form_developer_emailAddress")}
                               name="rm_email"
+                              value={devData?.rm_email}
                               className="w-full"
                               sx={{
                                 "&": {
-                                  marginBottom: "1.25rem !important",
+                                  marginBottom: "1rem !important",
                                 },
                               }}
                               variant="outlined"
                               size="small"
+                              onChange={handleEmail}
                             />
+
+                            {emailError?.rm_email && (
+                              <Typography
+                                variant="body2"
+                                color="error"
+                                className="mb-3"
+                              >
+                                {emailError?.rm_email}
+                              </Typography>
+                            )}
 
                             <TextField
                               id="LeadEmailAddress"
                               type={"text"}
                               label={t("form_developer_contactNumber")}
                               name="rm_contact"
+                              value={devData?.rm_contact}
                               className="w-full"
                               sx={{
                                 "&": {
@@ -433,6 +517,7 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
                               }}
                               variant="outlined"
                               size="small"
+                              onChange={handleChange}
                             />
                           </Box>
                         </div>
@@ -449,7 +534,7 @@ const AddDeveloper = ({ openAddDev, setOpenAddDev }) => {
                           type="submit"
                           disabled={loading ? true : false}
                         >
-                          {loading ? (
+                          {devLoading ? (
                             <CircularProgress
                               size={20}
                               sx={{ color: "white" }}
