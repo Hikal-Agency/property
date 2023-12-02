a50499f73395336f0c893209a71595ea8f66ef9a
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-12-02 12:08:08 +0500

    i18n fixed

--- src/index.js
+++ src/index.js
@@ -6,21 +6,24 @@ import "react-toastify/dist/ReactToastify.css";
 import { ContextProvider } from "./context/ContextProvider";
 import { ProSidebarProvider } from "react-pro-sidebar";
 import { BrowserRouter } from "react-router-dom";
-import './i18n'
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
     <ContextProvider>
-      <ProSidebarProvider>
-        {/* <Backdrop /> */}
-        <BrowserRouter>
-        <React.Suspense fallback="Loading...">
-      <App />
-    </React.Suspense>
-        </BrowserRouter>
-      </ProSidebarProvider>
+      <I18nextProvider i18n={i18n}>
+        <ProSidebarProvider>
+          {/* <Backdrop /> */}
+          <BrowserRouter>
+            <React.Suspense fallback="Loading...">
+              <App />
+            </React.Suspense>
+          </BrowserRouter>
+        </ProSidebarProvider>
+      </I18nextProvider>
     </ContextProvider>
   </>
 );
