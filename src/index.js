99300d38b7538042ac45ce1c1aeefd956d70b5c9
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-12-02 12:15:19 +0500

    index.js reverted

--- src/index.js
+++ src/index.js
@@ -6,24 +6,21 @@ import "react-toastify/dist/ReactToastify.css";
 import { ContextProvider } from "./context/ContextProvider";
 import { ProSidebarProvider } from "react-pro-sidebar";
 import { BrowserRouter } from "react-router-dom";
-import { I18nextProvider } from "react-i18next";
-import i18n from "./i18n";;
+import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
     <ContextProvider>
-      <I18nextProvider i18n={i18n}>
-        <ProSidebarProvider>
-          {/* <Backdrop /> */}
-          <BrowserRouter>
-            <React.Suspense fallback="Loading...">
-              <App />
-            </React.Suspense>
-          </BrowserRouter>
-        </ProSidebarProvider>
-      </I18nextProvider>
+      <ProSidebarProvider>
+        {/* <Backdrop /> */}
+        <BrowserRouter>
+        <React.Suspense fallback="Loading...">
+      <App />
+    </React.Suspense>
+        </BrowserRouter>
+      </ProSidebarProvider>
     </ContextProvider>
   </>
 );
