b51fe425d35396a4cb8dc1ff7d7a8ca443ce2e95
ubaidawan <malikunknown633@gmail.com>
2023-12-18 10:59:46 +0500

    working on clients listing

--- src/Components/OnBoardingComp/ClientsListComp.js
+++ src/Components/OnBoardingComp/ClientsListComp.js
@@ -1,8 +1,9 @@
 import { Card } from "@mui/material";
 import React from "react";
 import { useStateContext } from "../../context/ContextProvider";
-import { FaUser, FaPhone } from "react-icons/fa";
+import { FaUser, FaPhone, FaFacebookF } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
+import { ImUsers } from "react-icons/im";
 
 const ClientsListComp = () => {
   const { currentMode } = useStateContext();
@@ -40,23 +41,21 @@ const ClientsListComp = () => {
               Business Name
             </h3>
           </div>
-          <div className="flex items-center space-x-2 pr-3">
-            <span className="bg-primary p-3">
-              <h3
-                className={`${
-                  currentMode === "dark" && "text-white"
-                } text-bold`}
-              >
-                Account Type
-              </h3>
-            </span>
-            <h3
-              className={`${
-                currentMode === "dark" ? "text-white" : "text-dark"
-              }`}
+          <div className="flex items-center space-x-3 pr-3 ">
+            <span
+              className={`p-3 border rounded rounded-full ${
+                currentMode === "dark" ? "border-[#fff]" : "border-[#000]"
+              } cursor-pointer`}
             >
-              Business Name
-            </h3>
+              <FaFacebookF color="blue" />
+            </span>
+            <span
+              className={`p-3 border rounded rounded-full ${
+                currentMode === "dark" ? "border-[#fff]" : "border-[#000]"
+              } cursor-pointer`}
+            >
+              <FaFacebookF color="blue" />
+            </span>
           </div>
         </div>
 
@@ -159,7 +158,13 @@ const ClientsListComp = () => {
                 color={currentMode === "dark" ? "#fff" : "#000"}
               />
 
-              <p>Name of person to contact</p>
+              <p
+                className={`${
+                  currentMode === "dark" ? "text-white" : "text-dark"
+                }`}
+              >
+                Name of person to contact
+              </p>
             </div>
             <div className="flex space-between space-x-3 mt-3 items-center">
               <FaPhone
@@ -167,7 +172,13 @@ const ClientsListComp = () => {
                 color={currentMode === "dark" ? "#fff" : "#000"}
               />
 
-              <p>+9232312323132</p>
+              <p
+                className={`${
+                  currentMode === "dark" ? "text-white" : "text-dark"
+                }`}
+              >
+                +9232312323132
+              </p>
             </div>
             <div className="flex space-between space-x-3 mt-3 items-center">
               <IoIosMail
@@ -175,15 +186,27 @@ const ClientsListComp = () => {
                 color={currentMode === "dark" ? "#fff" : "#000"}
               />
 
-              <p>test@gmail.com</p>
+              <p
+                className={`${
+                  currentMode === "dark" ? "text-white" : "text-dark"
+                }`}
+              >
+                test@gmail.com
+              </p>
             </div>
             <div className="flex space-between space-x-3 mt-3 items-center">
-              <IoIosMail
+              <ImUsers
                 size={16}
                 color={currentMode === "dark" ? "#fff" : "#000"}
               />
 
-              <p>test@gmail.com</p>
+              <p
+                className={`${
+                  currentMode === "dark" ? "text-white" : "text-dark"
+                }`}
+              >
+                Number of users
+              </p>
             </div>
           </div>
         </div>
