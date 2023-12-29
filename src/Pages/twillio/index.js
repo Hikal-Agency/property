f3fea8d68f8f8fcd92ccf1eb8d069cb2bfd51772
ubaidawan <malikunknown633@gmail.com>
2023-12-29 11:07:34 +0500

    added messages in twillio form

--- src/Pages/twillio/index.js
+++ src/Pages/twillio/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useStateContext } from "../../context/ContextProvider";
 import { Box, Button, CircularProgress, TextField } from "@mui/material";
 import axios from "../../axoisConfig";
+import { toast } from "react-toastify";
 
 const Twillio = () => {
   const {
@@ -23,6 +24,19 @@ const Twillio = () => {
   const token = localStorage.getItem("auth-token");
 
   const integrateTwillio = async () => {
+    if (!twillioData?.twilio_id || !twillioData?.twilio_token) {
+      toast.error("All fields are required.", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      return;
+    }
     try {
       const twillioIntegration = await axios.post(
         `${BACKEND_URL}/store-tw-credentials`,
@@ -36,8 +50,28 @@ const Twillio = () => {
       );
 
       console.log("twillio integration response::: ", twillioIntegration);
+      toast.success("Your twillio account successfully integrated.", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     } catch (error) {
       console.log("Error=====> ", error);
+      toast.error("Unable to connect your account.", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     }
   };
   return (
@@ -49,7 +83,7 @@ const Twillio = () => {
         }}
         disabled={loading ? true : false}
       >
-        <div className="w-full flex items-center pb-3">
+        <div className="w-full flex items-center pb-3 mt-3">
           <div className="bg-primary h-10 w-1 rounded-full"></div>
           <h1
             className={`text-lg font-semibold mx-2 uppercase ${
@@ -60,102 +94,109 @@ const Twillio = () => {
           </h1>
         </div>
 
-        <div
-          className={`${
-            themeBgImg &&
-            (currentMode === "dark"
-              ? "blur-bg-dark shadow-sm"
-              : "blur-bg-light shadow-sm")
-          } p-5 rounded-lg `}
-        >
-          <div className=" mt-5">
-            <div className="px-4">
-              <Box
-                sx={{
-                  ...darkModeColors,
-                  "& .MuiFormLabel-root, .MuiInputLabel-root, .MuiInputLabel-formControl":
-                    {
-                      right: isLangRTL(i18n.language) ? "2.5rem" : "inherit",
-                      transformOrigin: isLangRTL(i18n.language)
-                        ? "right"
-                        : "left",
+        <div className="grid place-items-center h-auto">
+          <div
+            className={`${
+              themeBgImg &&
+              (currentMode === "dark"
+                ? // ? "blur-bg-dark shadow-sm"
+                  // : "blur-bg-light shadow-sm")
+                  "bg-blue-500 shadow-sm"
+                : "blur-bg-light shadow-sm")
+            } p-5 rounded-lg w-4/5 h-52  `}
+            style={{
+              background: currentMode === "dark" ? "#1c1c1c" : "#EEEEEE",
+            }}
+          >
+            <div className=" mt-5">
+              <div className="px-4">
+                <Box
+                  sx={{
+                    ...darkModeColors,
+                    "& .MuiFormLabel-root, .MuiInputLabel-root, .MuiInputLabel-formControl":
+                      {
+                        right: isLangRTL(i18n.language) ? "2.5rem" : "inherit",
+                        transformOrigin: isLangRTL(i18n.language)
+                          ? "right"
+                          : "left",
+                      },
+                    "& legend": {
+                      textAlign: isLangRTL(i18n.language) ? "right" : "left",
                     },
-                  "& legend": {
-                    textAlign: isLangRTL(i18n.language) ? "right" : "left",
-                  },
-                }}
-              >
-                <div className="flex justify-between space-x-4">
-                  <TextField
-                    id="id"
-                    type={"text"}
-                    label={t("twillio_id")}
-                    className="w-full"
-                    sx={{
-                      "&": {
-                        marginBottom: "1.25rem !important",
-                        zIndex: 1,
-                      },
-                    }}
-                    variant="outlined"
-                    size="small"
-                    value={twillioData?.twilio_id}
-                    onChange={(e) =>
-                      setTwillioData({
-                        ...twillioData,
-                        twilio_id: e.target.value,
-                      })
-                    }
-                    required
-                  />
-                  <TextField
-                    id="notes"
-                    type={"text"}
-                    label={t("twillio_token")}
-                    className="w-full"
-                    sx={{
-                      "&": {
-                        marginBottom: "1.25rem !important",
-                        zIndex: 1,
-                      },
-                    }}
-                    variant="outlined"
-                    size="small"
-                    value={twillioData?.twilio_token}
-                    onChange={(e) =>
-                      setTwillioData({
-                        ...twillioData,
-                        twilio_token: e.target.value,
-                      })
-                    }
-                    required
-                  />
-                </div>
-              </Box>
+                  }}
+                >
+                  <div className="flex justify-between space-x-4">
+                    <TextField
+                      id="id"
+                      type={"text"}
+                      label={t("twillio_id")}
+                      className="w-full"
+                      sx={{
+                        "&": {
+                          marginBottom: "1.25rem !important",
+                          zIndex: 1,
+                        },
+                      }}
+                      variant="outlined"
+                      size="small"
+                      value={twillioData?.twilio_id}
+                      onChange={(e) =>
+                        setTwillioData({
+                          ...twillioData,
+                          twilio_id: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <TextField
+                      id="notes"
+                      type={"text"}
+                      label={t("twillio_token")}
+                      className="w-full"
+                      sx={{
+                        "&": {
+                          marginBottom: "1.25rem !important",
+                          zIndex: 1,
+                        },
+                      }}
+                      variant="outlined"
+                      size="small"
+                      value={twillioData?.twilio_token}
+                      onChange={(e) =>
+                        setTwillioData({
+                          ...twillioData,
+                          twilio_token: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                  </div>
+                </Box>
+              </div>
             </div>
-          </div>
 
-          <div className="">
-            <Button
-              className={`min-w-fit w-full text-white rounded-md py-3 font-semibold disabled:opacity-50 disabled:cursor-not-allowed hover:shadow-none`}
-              ripple={true}
-              style={{
-                background: `${primaryColor}`,
-              }}
-              size="lg"
-              type="submit"
-              disabled={loading ? true : false}
-            >
-              {loading ? (
-                <CircularProgress
-                  size={20}
-                  sx={{ color: "white" }}
-                  className="text-white"
-                />
-              ) : (
-                <span className="text-white">{t("twillio_add")}</span>
-              )}
-            </Button>
+            <div className="">
+              <Button
+                className={`min-w-fit w-full text-white rounded-md py-3 font-semibold disabled:opacity-50 disabled:cursor-not-allowed hover:shadow-none`}
+                ripple={true}
+                style={{
+                  background: `${primaryColor}`,
+                }}
+                size="lg"
+                type="submit"
+                disabled={loading ? true : false}
+              >
+                {loading ? (
+                  <CircularProgress
+                    size={20}
+                    sx={{ color: "white" }}
+                    className="text-white"
+                  />
+                ) : (
+                  <span className="text-white">{t("twillio_add")}</span>
+                )}
+              </Button>
+            </div>
           </div>
         </div>
       </form>
