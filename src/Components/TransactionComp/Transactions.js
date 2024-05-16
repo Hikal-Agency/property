63a92d5a431275401ece91dcc4acfb5157fa8c05
ubaidawan <malikunknown633@gmail.com>
2024-05-16 10:35:44 +0500

    fixed text color and vat list positions and remove from new transactions  - finance

--- src/Components/TransactionComp/Transactions.js
+++ src/Components/TransactionComp/Transactions.js
@@ -735,81 +735,81 @@ const Transactions = ({ pathname }) => {
             >
               <span>{t("clear_all")}</span>
             </Button>
+
+            <div className="grid grid-cols-2 gap-5 mb-2  p-4 h-[200px] overflow-y-auto mt-4">
+              {vatData && vatData?.length > 0
+                ? vatData?.map((vat) => (
+                    <>
+                      {/* INCOME */}
+                      <div
+                        className={`rounded-xl shadow-sm w-full  flex flex-col justify-center gap-4 ${
+                          themeBgImg
+                            ? currentMode === "dark"
+                              ? "blur-bg-dark"
+                              : "blur-bg-white"
+                            : currentMode === "dark"
+                            ? "bg-[#1C1C1C]"
+                            : "bg-[#EEEEEE]"
+                        }`}
+                      >
+                        <p className="text-center bg-primary m-0 p-3 text-white">
+                          {t("income_amount")} - {vat?.year}
+                        </p>
+                        <div className="flex space-x-3 px-5">
+                          <p className={`text-center text-xl font-semibold`}>
+                            {t("vat")} :
+                          </p>
+                          <p className={`text-center text-xl  ml-3`}>
+                            {vat?.currency} {vat?.income_vat}
+                          </p>
+                        </div>
+                        <div className="flex space-x-3 px-5 mb-2">
+                          <p className={`text-center text-xl font-semibold`}>
+                            {t("amount")} :
+                          </p>
+                          <p className={`text-center text-xl  ml-3`}>
+                            {vat?.currency} {vat?.income_amount}
+                          </p>
+                        </div>
+                      </div>
+                      {/* EXPENSE */}
+                      <div
+                        className={`rounded-xl shadow-sm w-full  flex flex-col justify-center gap-4 ${
+                          themeBgImg
+                            ? currentMode === "dark"
+                              ? "blur-bg-dark"
+                              : "blur-bg-white"
+                            : currentMode === "dark"
+                            ? "bg-[#1C1C1C]"
+                            : "bg-[#EEEEEE]"
+                        }`}
+                      >
+                        <p className="text-center bg-primary m-0 p-3 text-white">
+                          {t("expense_amount")} - {vat?.year}
+                        </p>
+                        <div className="flex space-x-3 px-5">
+                          <p className={`text-center text-xl font-semibold`}>
+                            {t("vat")} :
+                          </p>
+                          <p className={`text-center text-xl  ml-3`}>
+                            {vat?.currency} {vat?.expense_vat}
+                          </p>
+                        </div>
+                        <div className="flex space-x-3 px-5 mb-2">
+                          <p className={`text-center text-xl font-semibold`}>
+                            {t("amount")} :
+                          </p>
+                          <p className={`text-center text-xl  ml-3`}>
+                            {vat?.currency} {vat?.expense_amount}
+                          </p>
+                        </div>
+                      </div>
+                    </>
+                  ))
+                : null}
+            </div>
           </Box>
         )}
-
-        <div className="grid grid-cols-2 gap-5 mb-2  p-4 h-[200px] overflow-y-auto">
-          {vatData && vatData?.length > 0
-            ? vatData?.map((vat) => (
-                <>
-                  {/* INCOME */}
-                  <div
-                    className={`rounded-xl shadow-sm w-full  flex flex-col justify-center gap-4 ${
-                      themeBgImg
-                        ? currentMode === "dark"
-                          ? "blur-bg-dark"
-                          : "blur-bg-white"
-                        : currentMode === "dark"
-                        ? "bg-[#1C1C1C]"
-                        : "bg-[#EEEEEE]"
-                    }`}
-                  >
-                    <p className="text-center bg-primary m-0 p-3">
-                      {t("income_amount")} - {vat?.year}
-                    </p>
-                    <div className="flex space-x-3 px-5">
-                      <p className={`text-center text-xl font-semibold`}>
-                        {t("vat")} :
-                      </p>
-                      <p className={`text-center text-xl  ml-3`}>
-                        {vat?.currency} {vat?.income_vat}
-                      </p>
-                    </div>
-                    <div className="flex space-x-3 px-5 mb-2">
-                      <p className={`text-center text-xl font-semibold`}>
-                        {t("amount")} :
-                      </p>
-                      <p className={`text-center text-xl  ml-3`}>
-                        {vat?.currency} {vat?.income_amount}
-                      </p>
-                    </div>
-                  </div>
-                  {/* EXPENSE */}
-                  <div
-                    className={`rounded-xl shadow-sm w-full  flex flex-col justify-center gap-4 ${
-                      themeBgImg
-                        ? currentMode === "dark"
-                          ? "blur-bg-dark"
-                          : "blur-bg-white"
-                        : currentMode === "dark"
-                        ? "bg-[#1C1C1C]"
-                        : "bg-[#EEEEEE]"
-                    }`}
-                  >
-                    <p className="text-center bg-primary m-0 p-3">
-                      {t("expense_amount")} - {vat?.year}
-                    </p>
-                    <div className="flex space-x-3 px-5">
-                      <p className={`text-center text-xl font-semibold`}>
-                        {t("vat")} :
-                      </p>
-                      <p className={`text-center text-xl  ml-3`}>
-                        {vat?.currency} {vat?.expense_vat}
-                      </p>
-                    </div>
-                    <div className="flex space-x-3 px-5 mb-2">
-                      <p className={`text-center text-xl font-semibold`}>
-                        {t("amount")} :
-                      </p>
-                      <p className={`text-center text-xl  ml-3`}>
-                        {vat?.currency} {vat?.expense_amount}
-                      </p>
-                    </div>
-                  </div>
-                </>
-              ))
-            : null}
-        </div>
       </div>
       {singleTransModal && (
         <SingleTransactionModal
