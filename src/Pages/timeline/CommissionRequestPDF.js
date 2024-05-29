c68b0dca5636fd21a396c6b8a3b21ccf1deddb1a
ubaidawan <malikunknown633@gmail.com>
2024-05-29 12:48:12 +0500

    working pdf design

--- src/Pages/timeline/CommissionRequestPDF.js
+++ src/Pages/timeline/CommissionRequestPDF.js
@@ -11,14 +11,18 @@ import {
 } from "@react-pdf/renderer";
 
 // Create styles
+
 const styles = StyleSheet.create({
   page: {
     fontFamily: "Helvetica",
-    fontSize: 10,
-    flexDirection: "column",
-    backgroundColor: "#FFFFFF",
+    fontSize: 11,
     padding: 30,
-    width: "100vw",
+    flexDirection: "column",
+  },
+  logo: {
+    width: 100,
+    height: "auto",
+    marginBottom: 20,
   },
   header: {
     display: "flex",
@@ -26,43 +30,55 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     marginBottom: 20,
   },
-  companyInfo: {
-    textAlign: "right",
+  title: {
+    fontSize: 20,
+    marginBottom: 10,
+    textAlign: "center",
   },
   table: {
     display: "table",
+    // flexDirection: "column",
     width: "auto",
-    marginVertical: 10,
-    borderWidth: 1,
-    borderColor: "#000",
     borderStyle: "solid",
+    borderWidth: 1,
+    borderRightWidth: 0,
+    borderBottomWidth: 0,
+    marginBottom: 20,
   },
   tableRow: {
-    flexDirection: "row",
+    flexDirection: "column",
   },
   tableColHeader: {
     width: "20%",
+    borderStyle: "solid",
     borderWidth: 1,
-    borderColor: "#000",
-    backgroundColor: "#e0e0e0",
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+    backgroundColor: "#f0f0f0",
     padding: 5,
     textAlign: "center",
   },
   tableCol: {
     width: "20%",
+    borderStyle: "solid",
     borderWidth: 1,
-    borderColor: "#000",
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
     padding: 5,
     textAlign: "center",
   },
-  total: {
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    marginTop: 20,
-    fontWeight: "bold",
-  },
   footer: {
-    marginTop: 20,
+    position: "absolute",
+    bottom: 30,
+    left: 30,
+    right: 30,
+    textAlign: "center",
+  },
+  contactInfo: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 30,
   },
 });
 
@@ -70,34 +86,93 @@ const styles = StyleSheet.create({
 export const CommissionRequestPDF = ({ data }) => {
   console.log("data for pdf", data);
   return (
+    // <Document>
+    //   <Page size="A4" style={styles.page}>
+    //     <View style={styles.header}>
+    //       <View>
+    //         <Text>Bill to:</Text>
+    //         <Text>{data?.vendor_name}</Text>
+    //         <Text>{data?.address}</Text>
+    //         <Text>TRN No: {data?.trn}</Text>
+    //       </View>
+    //       <View style={styles.companyInfo}>
+    //         <Text>Company: {data?.company}</Text>
+    //         <Text>TRN No: {data?.company_trn}</Text>
+    //         <Text>Email Address: {data?.company_email}</Text>
+    //         <Text>Telephone: {data?.company_tele}</Text>
+    //       </View>
+    //     </View>
+
+    //     <Text
+    //       style={{
+    //         textAlign: "center",
+    //         fontWeight: "bold",
+    //         fontSize: 20,
+    //         marginBottom: 20,
+    //         textDecoration: "underline",
+    //       }}
+    //     >
+    //       TAX INVOICE
+    //     </Text>
+
+    //     <View style={styles.table}>
+    //       <View style={styles.tableRow}>
+    //         <Text style={styles.tableColHeader}>CLIENT NAME</Text>
+    //         <Text style={styles.tableColHeader}>UNIT NO</Text>
+    //         <Text style={styles.tableColHeader}>PROJECT NAME</Text>
+    //       </View>
+    //       <View style={styles.tableRow}>
+    //         <Text style={styles.tableCol}>{data?.leadName}</Text>
+    //         <Text style={styles.tableCol}>{data?.unit}</Text>
+    //         <Text style={styles.tableCol}>{data?.project}</Text>
+    //       </View>
+    //     </View>
+
+    //     <View style={styles.table}>
+    //       <View style={styles.tableRow}>
+    //         <Text style={styles.tableColHeader}>SALES VALUE (AED)</Text>
+    //         <Text style={styles.tableColHeader}>COMMISSION %</Text>
+    //         <Text style={styles.tableColHeader}>NET VALUE BEFORE VAT</Text>
+    //         <Text style={styles.tableColHeader}>VAT VALUE</Text>
+    //         <Text style={styles.tableColHeader}>GROSS VALUE (AED)</Text>
+    //       </View>
+    //       <View style={styles.tableRow}>
+    //         <Text style={styles.tableCol}>{data?.amount}</Text>
+    //         <Text style={styles.tableCol}>{data?.comm_percent}</Text>
+    //         <Text style={styles.tableCol}>{data?.comm_amount}</Text>
+    //         <Text style={styles.tableCol}>5% of Selling</Text>
+    //         <Text style={styles.tableCol}>Gross Value</Text>
+    //       </View>
+    //     </View>
+
+    //     <View style={styles.total}>
+    //       <Text>TOTAL: Gross Value</Text>
+    //     </View>
+
+    //     <View style={styles.footer}>
+    //       <Text>All cheques payable to the following account:</Text>
+    //       <Text>Bank Name: {data?.bank_name}</Text>
+    //       <Text>Bank Address: {data?.bank_address}</Text>
+    //       <Text>Bank Account Name: {data?.bank_acc_name}</Text>
+    //       <Text>Account Number: {data?.bank_acc_no}</Text>
+    //       <Text>IBAN: {data?.bank_iban}</Text>
+    //       <Text>SWIFT Code: {data?.bank_swift_code}</Text>
+    //       <Text>Sincerely,</Text>
+    //       <Text>Mr. MOHAMED MEDHAT FATHY IBRAHIM HIKAL</Text>
+    //       <Text>CEO</Text>
+    //       <Text>HIKAL REAL ESTATE L.L.C</Text>
+    //     </View>
+    //   </Page>
+    // </Document>
+
     <Document>
       <Page size="A4" style={styles.page}>
         <View style={styles.header}>
-          <View>
-            <Text>Bill to:</Text>
-            <Text>{data?.vendor_name}</Text>
-            <Text>{data?.address}</Text>
-            <Text>TRN No: {data?.trn}</Text>
-          </View>
-          <View style={styles.companyInfo}>
-            <Text>Company: {data?.company}</Text>
-            <Text>TRN No: {data?.company_trn}</Text>
-            <Text>Email Address: {data?.company_email}</Text>
-            <Text>Telephone: {data?.company_tele}</Text>
-          </View>
+          <Text>HIKAL REAL ESTATE</Text>
+          <Text>Contact Info: +971 4 272 2249 | info@hikalagency.ae</Text>
         </View>
 
-        <Text
-          style={{
-            textAlign: "center",
-            fontWeight: "bold",
-            fontSize: 20,
-            marginBottom: 20,
-            textDecoration: "underline",
-          }}
-        >
-          TAX INVOICE
-        </Text>
+        <Text style={styles.title}>TAX INVOICE</Text>
 
         <View style={styles.table}>
           <View style={styles.tableRow}>
@@ -106,9 +181,9 @@ export const CommissionRequestPDF = ({ data }) => {
             <Text style={styles.tableColHeader}>PROJECT NAME</Text>
           </View>
           <View style={styles.tableRow}>
-            <Text style={styles.tableCol}>{data?.leadName}</Text>
-            <Text style={styles.tableCol}>{data?.unit}</Text>
-            <Text style={styles.tableCol}>{data?.project}</Text>
+            <Text style={styles.tableCol}>ClientName</Text>
+            <Text style={styles.tableCol}>UnitNumber</Text>
+            <Text style={styles.tableCol}>ProjectName</Text>
           </View>
         </View>
 
@@ -121,30 +196,38 @@ export const CommissionRequestPDF = ({ data }) => {
             <Text style={styles.tableColHeader}>GROSS VALUE (AED)</Text>
           </View>
           <View style={styles.tableRow}>
-            <Text style={styles.tableCol}>{data?.amount}</Text>
-            <Text style={styles.tableCol}>{data?.comm_percent}</Text>
-            <Text style={styles.tableCol}>{data?.comm_amount}</Text>
-            <Text style={styles.tableCol}>5% of Selling</Text>
-            <Text style={styles.tableCol}>Gross Value</Text>
+            <Text style={styles.tableCol}>SellingAmount</Text>
+            <Text style={styles.tableCol}>CommPercent</Text>
+            <Text style={styles.tableCol}>CommAmount</Text>
+            <Text style={styles.tableCol}>VATValue</Text>
+            <Text style={styles.tableCol}>GrossValue</Text>
           </View>
         </View>
 
-        <View style={styles.total}>
-          <Text>TOTAL: Gross Value</Text>
+        <View>
+          <Text>All cheques payable to the following account:</Text>
+          <Text>Bank Name: EMIRATES ISLAMIC</Text>
+          <Text>Bank Address: EL SHK ZAYED RD AL WASL TOWER</Text>
+          <Text>Bank Account Name: HIKAL REAL ESTATE L.L.C.</Text>
+          <Text>Account Number: 3708453323701</Text>
+          <Text>IBAN: AE31043007078453323701</Text>
+          <Text>SWIFT Code: MEBLAEAD</Text>
         </View>
 
         <View style={styles.footer}>
-          <Text>All cheques payable to the following account:</Text>
-          <Text>Bank Name: {data?.bank_name}</Text>
-          <Text>Bank Address: {data?.bank_address}</Text>
-          <Text>Bank Account Name: {data?.bank_acc_name}</Text>
-          <Text>Account Number: {data?.bank_acc_no}</Text>
-          <Text>IBAN: {data?.bank_iban}</Text>
-          <Text>SWIFT Code: {data?.bank_swift_code}</Text>
           <Text>Sincerely,</Text>
           <Text>Mr. MOHAMED MEDHAT FATHY IBRAHIM HIKAL</Text>
           <Text>CEO</Text>
-          <Text>HIKAL REAL ESTATE L.L.C</Text>
+          <Text>HIKAL REAL ESTATE L.L.C.</Text>
+          <Text>Authorized Signature</Text>
+        </View>
+
+        <View style={styles.contactInfo}>
+          <Text>+971 4 272 2249 | info@hikalagency.ae</Text>
+          <Text>
+            Office No. 2704, API World Tower, Sheikh Zayed Road, Dubai
+          </Text>
+          <Text>www.hikalproperties.com</Text>
         </View>
       </Page>
     </Document>
