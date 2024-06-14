c10b8890b2aef7d2c53166a9185504601687c813
ubaidawan <malikunknown633@gmail.com>
2024-06-14 17:49:05 +0500

    show relevant heading for agent and manager in commission modal

--- src/Pages/timeline/SendSMSNotif.js
+++ src/Pages/timeline/SendSMSNotif.js
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
-import axios from "../../axoisConfig";
-import { toast } from "react-toastify";
+// import axios from "../../axoisConfig";
 import { boss } from "../../Components/_elements/SelectOptions";
+import axios from "axios";
 
 export const sendSMSNotif = async (t, BACKEND_URL, data) => {
   console.log("sms data: ", data);
   const token = localStorage.getItem("auth-token");
-  const phoneNumber = boss(t)?.find((boss) => boss?.id == 102)?.phone;
+  // const phoneNumber = boss(t)?.find((boss) => boss?.id == 102)?.phone;
+  const phoneNumber = "+971563110950";
   console.log("phone number: ", phoneNumber);
   const smsData = {
     phone_number: phoneNumber,
     senderAddr: "AD-HIKAL",
-    message: `Commission for ${data?.project} ${data?.enquiryType} unit ${data?.unit} ,has been received.`,
+    message: `Commission for ${data?.project} ${data?.enquiryType} unit ${data?.unit}, has been received.`,
     type: "sms",
   };
   try {
     const sendSMS = await axios.post(`${BACKEND_URL}/otp`, smsData, {
       headers: {
-        "Content-Type": "multipart/form-data",
+        "Content-Type": "application/json",
         Authorization: "Bearer " + token,
       },
     });
