efd36b8da7ea138a65fbd1cadb2fb8a94dc461fd
Muhammad Hussain <7phussain@gmail.com>
2024-07-15 10:49:05 +0500

    dark mode issue speech to text

--- src/Components/_elements/LanguageDetectModal.js
+++ src/Components/_elements/LanguageDetectModal.js
@@ -8,8 +8,10 @@ import {
   Modal,
   TextField,
   MenuItem,
-  Select,
+  // Select,
 } from "@mui/material";
+import Select from "react-select";
+import { selectStyles } from "./SelectStyles";
 
 import { IoMdClose } from "react-icons/io";
 import { GoDuplicate } from "react-icons/go";
@@ -83,31 +85,73 @@ const LanguageDetectModal = ({
       <div
         style={style}
         className={`w-[calc(30%-20px)] md:w-[30%]  ${
-          currentMode === "dark" ? "bg-[#1c1c1c]" : "bg-white"
+          // currentMode === "dark" ? "bg-[#1c1c1c]" : "bg-white"
+          currentMode === "dark"
+            ? "bg-dark-neu text-white"
+            : "bg-light text-black"
         } absolute top-1/2 left-1/2 p-5 rounded-md`}
       >
-        <h3 className="text-2xl mb-6 mt-3 font-medium text-gray-700">
-          Select Language For Speech to Text{" "}
+        <h3
+          className={`text-2xl mb-6 mt-3 font-medium  ${
+            currentMode === "dark" ? "text-white" : "text-gray-700"
+          } `}
+        >
+          Select Language For Speech to Text
           <span className="text-red-600">*</span>
         </h3>
-        <Select
+        {/* <Select
           id="languageDetect"
           value={language}
-          //   label="Language"
+          // label="Language"
+          placeholder="Select Language"
           onChange={(e) => {
             setLanguage(e?.target?.value);
             setIsVoiceSearchState((pre) => !pre);
             setLanguageModal(false);
           }}
+          onBlur={(e) => {
+            setIsVoiceSearchState((pre) => !pre);
+            setLanguageModal(false);
+          }}
           size="medium"
           className="w-full"
-          displayEmpty
+          // displayEmpty
           required
         >
           {languages?.map((lang) => {
-            return <MenuItem value={lang?.code}>{lang?.name}</MenuItem>;
+            return (
+              <MenuItem
+                value={lang?.code}
+                className={`${
+                  currentMode == "dark" ? "text-white" : "text-black"
+                }`}
+              >
+                {lang?.name}
+              </MenuItem>
+            );
           })}
-        </Select>
+        </Select> */}
+        <Select
+          aria-label="select Manager"
+          id="select-language"
+          options={languages.map((language) => ({
+            value: language?.code,
+            label: language?.name,
+          }))}
+          value={languages?.filter((lang) => {
+            return lang?.code == language;
+          })}
+          // isDisabled=
+          onChange={(e) => {
+            setLanguage(e?.value);
+            setIsVoiceSearchState((pre) => !pre);
+            setLanguageModal(false);
+          }}
+          placeholder={t("Select Language")}
+          className={`mb-5`}
+          menuPortalTarget={document.body}
+          styles={selectStyles(currentMode, primaryColor)}
+        />
       </div>
     </Modal>
   );
