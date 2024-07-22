98ddde35649deb3d382e3f946c1be4590c644009
ubaidawan <malikunknown633@gmail.com>
2024-07-22 19:14:42 +0500

    added thumbnail option in add listing form

--- src/Components/Listings/listingFormComp/Addlisting.js
+++ src/Components/Listings/listingFormComp/Addlisting.js
@@ -1,5 +1,5 @@
 import { Box, Button, CircularProgress, TextField } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useStateContext } from "../../../context/ContextProvider";
 import Select from "react-select";
 import { selectStyles } from "../../../Components/_elements/SelectStyles";
@@ -23,26 +23,137 @@ const Addlisting = ({ data }) => {
   const token = localStorage.getItem("auth-token");
 
   const [btnLoading, setBtnLoading] = useState(false);
+  const [countryList, setCountryList] = useState([]);
+  const [cityList, setCityList] = useState([]);
+  const [stateList, setStateList] = useState([]);
+  const [cityLoading, setCityLoading] = useState(false);
+
   const [listingData, setlistingData] = useState({
     title: "",
-    meta_tags_for_listings_id: 2,
+    // meta_tags_for_listings_id: 2,
     listing_type_id: "",
     user_id: 3,
     listing_attribute_id: "",
     listing_arrtibute_type_id: "",
     country_id: 1,
-    state_id: 2,
-    city_id: 2,
+    state_id: "",
+    city_id: "",
     short_description: "",
     thumbnail: "",
     status: 1,
   });
+
+  console.log("listing data :: ", listingData);
 
   const handleChange = (e) => {
     setlistingData((prevListingAttr) => ({
       ...prevListingAttr,
       [e.target.id]: e.target.value,
     }));
+  };
+
+  const fetchCountries = () => {
+    axios
+      .get(`${BACKEND_URL}/countries`, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((result) => {
+        console.log("countries list : ", result);
+        setCountryList(result?.data?.data?.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        setBtnLoading(false);
+        console.log(err);
+        const errors = err.response?.data?.errors;
+
+        if (errors) {
+          const errorMessages = Object.values(errors).flat().join(" ");
+          toast.error(`Errors: ${errorMessages}`, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        } else {
+          toast.error("Unable to fetch countries", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+      });
+  };
+
+  const FetchCitynState = () => {
+    setCityLoading(true);
+
+    const country_id = listingData.country_id;
+
+    const citiesApi = axios.get(`${BACKEND_URL}/cities`, {
+      params: { country_id },
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+
+    const statesApi = axios.get(`${BACKEND_URL}/states`, {
+      params: { country_id },
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+
+    Promise.all([citiesApi, statesApi])
+      .then(([citiesResult, statesResult]) => {
+        console.log("Cities: ", citiesResult.data);
+        console.log("States: ", statesResult.data);
+
+        setCityList(citiesResult?.data?.data);
+        setStateList(statesResult?.data?.data);
+        setCityLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setCityLoading(false);
+        const error = err.response?.data?.message;
+
+        if (error) {
+          toast.error(` ${error}`, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        } else {
+          toast.error("Unable to fetch city and state", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+      });
   };
 
   const AddListings = () => {
@@ -114,6 +225,15 @@ const Addlisting = ({ data }) => {
       });
   };
 
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+  useEffect(() => {
+    if (listingData?.country_id) {
+      FetchCitynState();
+    }
+  }, [listingData?.country_id]);
+
   return (
     <div className="my-4">
       <h4 className={`text-primary text-center font-semibold pb-5`}>
@@ -135,7 +255,7 @@ const Addlisting = ({ data }) => {
           }}
         >
           <TextField
-            id="name"
+            id="title"
             type={"text"}
             label={t("title")}
             className="w-full"
@@ -144,9 +264,9 @@ const Addlisting = ({ data }) => {
             }}
             variant="outlined"
             size="small"
-            // value={projectData?.projectLocation}
+            value={listingData?.title}
             name="projectLocation"
-            // onChange={handleChange}
+            onChange={handleChange}
             required
           />
           <Select
@@ -177,46 +297,49 @@ const Addlisting = ({ data }) => {
           />
 
           <Select
-            id="Developer"
-            // value={{
-            //   value: projectData?.developer_id,
-            //   label: projectData?.developer_id
-            //     ? developer.find((dev) => dev.id === projectData?.developer_id)
-            //         ?.developerName || ""
-            //     : t("form_developer_name"),
-            // }}
-            // onChange={(selectedOption) => {
-            //   handleChange({
-            //     target: { name: "developer_id", value: selectedOption.value },
-            //   });
-            // }}
-            // options={developer.map((dev) => ({
-            //   value: dev.id,
-            //   label: dev.developerName,
-            // }))}
+            id="country_id"
+            value={{
+              value: listingData?.country_id,
+              label: listingData?.country_id
+                ? countryList?.find(
+                    (cont) => cont.id === listingData?.country_id
+                  )?.name || ""
+                : t("label_country"),
+            }}
+            onChange={(e) => {
+              setlistingData({
+                ...listingData,
+                country_id: e.value,
+              });
+            }}
+            options={countryList?.map((cont) => ({
+              value: cont.id,
+              label: cont.name,
+            }))}
             className="w-full"
             placeholder={t("label_country")}
             menuPortalTarget={document.body}
             styles={selectStyles(currentMode, primaryColor)}
           />
           <Select
-            id="Developer"
-            // value={{
-            //   value: projectData?.developer_id,
-            //   label: projectData?.developer_id
-            //     ? developer.find((dev) => dev.id === projectData?.developer_id)
-            //         ?.developerName || ""
-            //     : t("form_developer_name"),
-            // }}
-            // onChange={(selectedOption) => {
-            //   handleChange({
-            //     target: { name: "developer_id", value: selectedOption.value },
-            //   });
-            // }}
-            // options={developer.map((dev) => ({
-            //   value: dev.id,
-            //   label: dev.developerName,
-            // }))}
+            id="city_id"
+            value={{
+              value: listingData?.city_id,
+              label: listingData?.city_id
+                ? cityList?.find((cont) => cont.id === listingData?.city_id)
+                    ?.name || ""
+                : t("label_city"),
+            }}
+            onChange={(e) => {
+              setlistingData({
+                ...listingData,
+                city_id: e.value,
+              });
+            }}
+            options={cityList?.map((cont) => ({
+              value: cont.id,
+              label: cont.name,
+            }))}
             className="w-full"
             placeholder={t("label_city")}
             menuPortalTarget={document.body}
@@ -314,22 +437,23 @@ const Addlisting = ({ data }) => {
 
           <Select
             id="Developer"
-            // value={{
-            //   value: projectData?.developer_id,
-            //   label: projectData?.developer_id
-            //     ? developer.find((dev) => dev.id === projectData?.developer_id)
-            //         ?.developerName || ""
-            //     : t("form_developer_name"),
-            // }}
-            // onChange={(selectedOption) => {
-            //   handleChange({
-            //     target: { name: "developer_id", value: selectedOption.value },
-            //   });
-            // }}
-            // options={developer.map((dev) => ({
-            //   value: dev.id,
-            //   label: dev.developerName,
-            // }))}
+            value={{
+              value: listingData?.state_id,
+              label: listingData?.state_id
+                ? stateList?.find((cont) => cont.id === listingData?.state_id)
+                    ?.name || ""
+                : t("state"),
+            }}
+            onChange={(e) => {
+              setlistingData({
+                ...listingData,
+                state_id: e.value,
+              });
+            }}
+            options={stateList?.map((cont) => ({
+              value: cont.id,
+              label: cont.name,
+            }))}
             className="w-full"
             placeholder={t("state")}
             menuPortalTarget={document.body}
@@ -351,6 +475,43 @@ const Addlisting = ({ data }) => {
             onChange={handleChange}
             required
           />
+
+          <input
+            accept="image/*"
+            style={{ display: "none" }}
+            id="list-thumb"
+            type="file"
+            name="picture"
+            onChange={(e) => {
+              console.log("event of og image: ", e);
+
+              setlistingData({
+                ...listingData,
+                thumbnail: e.target.files,
+              });
+            }}
+          />
+
+          <label htmlFor="list-thumb">
+            <Button
+              variant="contained"
+              size="lg"
+              className="bg-main-red-color w-full bg-btn-primary  text-white rounded-lg py-3 border-primary font-semibold my-3 "
+              style={{
+                fontFamily: fontFam,
+                color: "#ffffff",
+                marginTop: "20px",
+              }}
+              component="span"
+              // disabled={loading ? true : false}
+              // startIcon={loading ? null : <MdFileUpload />}
+            >
+              <span>{t("thumbnail")}</span>
+            </Button>
+            <p className="text-primary mt-2 italic">
+              {listingData?.thumbnail ? `thumbnail selected.` : null}
+            </p>
+          </label>
 
           <Button
             className={`min-w-fit text-white rounded-md py-3 font-semibold disabled:opacity-50 disabled:cursor-not-allowed hover:shadow-none`}
